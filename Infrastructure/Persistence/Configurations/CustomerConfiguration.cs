006b3313cf33c0d768acf1d1aa07160e406d0341
Nik.Brosko <Nikita.brosko@mail.ru>
2022-04-15 20:15:09 +0300

    Update Customer entity Create CustomerConfiguration

---
+++ Infrastructure/Persistence/Configurations/CustomerConfiguration.cs
@@ -0,0 +1,60 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configurations
+{
+    public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.HasIndex(c => new { c.FirstName, c.LastName })
+                .IsUnique();
+            
+            builder.Property(c => c.FirstName)
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(c => c.LastName)
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(c => c.FullName)
+                .HasComputedColumnSql("[LastName] + ' ' + [FirstName]");
+
+            builder.Property(c => c.Address)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.Property(c => c.City)
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(c => c.Country)
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(c => c.Phone)
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(c => c.Email)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.Property(c => c.CreditCardNumber)
+                .HasMaxLength(16)
+                .IsRequired();
+
+            builder.Property(c => c.CardExpMonth)
+                .HasMaxLength(2)
+                .IsRequired();
+
+            builder.Property(c => c.CardExpYear)
+                .HasMaxLength(4)
+                .IsRequired();
+        }
+    }
+}
