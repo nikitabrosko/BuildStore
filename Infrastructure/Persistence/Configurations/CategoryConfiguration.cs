6631ab5dd404bf50191e58805f7f2b18731312ad
Nik.Brosko <Nikita.brosko@mail.ru>
2022-04-15 20:17:20 +0300

    Create CategoryConfiguration

---
+++ Infrastructure/Persistence/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.HasIndex(c => c.Name)
+                .IsUnique();
+
+            builder.Property(c => c.Name)
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(c => c.Description)
+                .HasMaxLength(500)
+                .IsRequired();
+
+            builder.Property(c => c.Picture)
+                .IsRequired();
+
+            builder.HasMany(c => c.Products)
+                .WithMany(p => p.Categories);
+        }
+    }
+}
