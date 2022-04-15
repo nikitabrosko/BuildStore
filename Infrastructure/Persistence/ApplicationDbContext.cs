372c3a2afbcd0a03ecfcafe93b19804d3f544c16
Nik.Brosko <Nikita.brosko@mail.ru>
2022-04-15 20:17:38 +0300

    Create ApplicationDbContext

---
+++ Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,45 @@
+﻿using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Customer> Customers => Set<Customer>();
+
+        public DbSet<Category> Categories => Set<Category>();
+
+        public DbSet<Delivery> Deliveries => Set<Delivery>();
+
+        public DbSet<Inventory> Inventories => Set<Inventory>();
+
+        public DbSet<Order> Orders => Set<Order>();
+
+        public DbSet<Payment> Payments => Set<Payment>();
+
+        public DbSet<Product> Products => Set<Product>();
+
+        public DbSet<Supplier> Suppliers => Set<Supplier>();
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
