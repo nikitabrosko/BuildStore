2b94db3e0763ecabe0ffc6be9841aae0509fcc89
Nik.Brosko <Nikita.brosko@mail.ru>
2022-04-16 15:35:56 +0300

    Add Interfaces to Application layer

---
+++ Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Common.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Category> Categories { get; }
+
+        DbSet<Customer> Customers { get; }
+
+        DbSet<Delivery> Deliveries { get; }
+
+        DbSet<Inventory> Inventories { get; }
+
+        DbSet<Order> Orders { get; }
+
+        DbSet<Payment> Payments { get; }
+
+        DbSet<Product> Products { get; }
+
+        DbSet<Supplier> Suppliers { get; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
