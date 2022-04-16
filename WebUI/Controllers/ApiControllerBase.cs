1fac30bf615879e805221a00f6731ce5cccd53a8
Nik.Brosko <Nikita.brosko@mail.ru>
2022-04-16 20:31:31 +0300

    Create ApiControllerBase class Create CategoryController class

---
+++ WebUI/Controllers/ApiControllerBase.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebUI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller][action]")]
+    public abstract class ApiControllerBase : Controller
+    {
+        private ISender _mediator;
+
+        protected ISender Mediator => _mediator ??= HttpContext.RequestServices.GetRequiredService<ISender>();
+    }
+}
