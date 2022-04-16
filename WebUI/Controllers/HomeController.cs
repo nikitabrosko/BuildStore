2defbb5a23ad1f53a82305aac1d158543e19539c
Nik.Brosko <Nikita.brosko@mail.ru>
2022-04-16 20:31:11 +0300

    Create default view's and Home controller

---
+++ WebUI/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.Controllers
+{
+    public class HomeController : ApiControllerBase
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Home page";
+
+            return View();
+        }
+    }
+}
