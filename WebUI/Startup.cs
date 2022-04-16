a13f969c948a24d1c3a17229d52c4a9290bbc563
Nik.Brosko <Nikita.brosko@mail.ru>
2022-04-16 20:30:16 +0300

    Update appsettings.json (add connection string) Update Startup class (add dependencies)

---
+++ WebUI/Startup.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Application;
+using FluentValidation.AspNetCore;
+using Infrastructure;
+using Microsoft.Extensions.Configuration;
+
+namespace WebUI
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddApplication();
+            services.AddInfrastructure(Configuration);
+
+            services.AddControllersWithViews()
+                .AddFluentValidation(x => x.AutomaticValidationEnabled = false);
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
