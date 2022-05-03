5b14ca84261b9374babe7350ef8278a1f6c57b13
Nik.Brosko <Nikita.brosko@mail.ru>
2022-05-03 23:48:55 +0300

    Create SubcategoryController

---
+++ WebUI/Controllers/SubcategoryController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using Application.UseCases.Subcategory.Commands.CreateSubcategory;
+using Application.UseCases.Subcategory.Queries.GetSubcategory;
+
+namespace WebUI.Controllers
+{
+    public class SubcategoryController : ApiControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetSubcategory([FromRoute] int id)
+        {
+            try
+            {
+                var subcategory = await Mediator.Send(new GetSubcategoryQuery { Id = id });
+
+                ViewBag.Title = subcategory.Name;
+
+                return View(subcategory);
+            }
+            catch (NotFoundException exception)
+            {
+                return View("Error", exception.Message);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewBag.Title = "Create Subcategory";
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromForm] CreateSubcategoryCommand command)
+        {
+            try
+            {
+                await Mediator.Send(command);
+            }
+            catch (ItemExistsException exception)
+            {
+                return View("Error", exception.Message);
+            }
+
+            return RedirectToAction("GetCategory", "Category");
+        }
+    }
+}
