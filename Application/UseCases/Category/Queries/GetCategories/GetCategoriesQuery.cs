187ce55dc47f602de3c636903e526e13b6ac12fb
Nik.Brosko <Nikita.brosko@mail.ru>
2022-05-03 23:47:10 +0300

    Create GetCategoriesQuery

---
+++ Application/UseCases/Category/Queries/GetCategories/GetCategoriesQuery.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using MediatR;
+
+namespace Application.UseCases.Category.Queries.GetCategories
+{
+    public class GetCategoriesQuery : IRequest<IEnumerable<Domain.Entities.Category>>
+    {
+        
+    }
+}
---
+++ Application/UseCases/Category/Queries/GetCategories/GetCategoriesQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.UseCases.Category.Queries.GetCategories
+{
+    public class GetCategoriesQueryHandler : IRequestHandler<GetCategoriesQuery, IEnumerable<Domain.Entities.Category>>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetCategoriesQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Domain.Entities.Category>> Handle(GetCategoriesQuery request, CancellationToken cancellationToken)
+        {
+            return await _context.Categories.ToListAsync(cancellationToken);
+        }
+    }
+}
