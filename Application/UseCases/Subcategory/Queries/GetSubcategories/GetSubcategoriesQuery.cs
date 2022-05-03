591eaae062ece2091bf3f506feb2a5d790ef6397
Nik.Brosko <Nikita.brosko@mail.ru>
2022-05-03 23:48:10 +0300

    Create GetSubcategoriesQuery

---
+++ Application/UseCases/Subcategory/Queries/GetSubcategories/GetSubcategoriesQuery.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using MediatR;
+
+namespace Application.UseCases.Subcategory.Queries.GetSubcategories
+{
+    public class GetSubcategoriesQuery : IRequest<IEnumerable<Domain.Entities.Subcategory>>
+    {
+        
+    }
+}
---
+++ Application/UseCases/Subcategory/Queries/GetSubcategories/GetSubcategoriesQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.UseCases.Subcategory.Queries.GetSubcategories
+{
+    public class GetSubcategoriesQueryHandler : IRequestHandler<GetSubcategoriesQuery, IEnumerable<Domain.Entities.Subcategory>>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetSubcategoriesQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Domain.Entities.Subcategory>> Handle(GetSubcategoriesQuery request, CancellationToken cancellationToken)
+        {
+            return await _context.Subcategories.ToListAsync(cancellationToken);
+        }
+    }
+}
