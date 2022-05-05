f5a6333e2f76e9984cf11f3ea73089f4a782eb4b
Nik.Brosko <Nikita.brosko@mail.ru>
2022-05-05 21:12:02 +0300

    Create DeleteSubcategoryCommand

---
+++ Application/UseCases/Subcategory/Commands/DeleteSubcategory/DeleteSubcategoryCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace Application.UseCases.Subcategory.Commands.DeleteSubcategory
+{
+    public class DeleteSubcategoryCommand : IRequest<int>
+    {
+        public int Id { get; set; }
+
+        public bool SubcategoriesDeletion { get; set; }
+
+        public bool ProductsDeletion { get; set; }
+    }
+}
---
+++ Application/UseCases/Subcategory/Commands/DeleteSubcategory/DeleteSubcategoryCommandHandler.cs
@@ -0,0 +1,78 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.UseCases.Subcategory.Commands.DeleteSubcategory
+{
+    public class DeleteSubcategoryCommandHandler : IRequestHandler<DeleteSubcategoryCommand, int>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public DeleteSubcategoryCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Handle(DeleteSubcategoryCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.Categories
+                .OfType<Domain.Entities.Subcategory>()
+                .Include(c => c.Category)
+                .Include(c => (c.Category as Domain.Entities.Subcategory).Category)
+                .Include(c => c.Products)
+                .Include(c => c.Subcategories)
+                .SingleOrDefaultAsync(c => c.Id.Equals(request.Id), cancellationToken);
+
+            if (entity is null)
+            {
+                throw new NotFoundException(nameof(Subcategory), request.Id);
+            }
+
+            if (request.ProductsDeletion)
+            {
+                _context.Products.RemoveRange(entity.Products);
+            }
+            else
+            {
+                foreach (var entityProduct in entity.Products)
+                {
+                    entityProduct.Category = null;
+                }
+            }
+
+            if (request.SubcategoriesDeletion)
+            {
+                _context.Categories.RemoveRange(entity.Subcategories);
+            }
+            else
+            {
+                foreach (var entitySubcategories in entity.Subcategories)
+                {
+                    entitySubcategories.Category = null;
+                }
+            }
+
+            int categoryId;
+
+            if (entity.Category is Domain.Entities.Subcategory subcategory)
+            {
+                categoryId = subcategory.Category.Id;
+            }
+            else
+            {
+                categoryId = entity.Category.Id;
+            }
+
+            _context.Categories.Remove(entity);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return categoryId;
+        }
+    }
+}
