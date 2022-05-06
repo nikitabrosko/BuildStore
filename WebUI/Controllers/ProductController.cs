e70e34449b2762081bccc09f446b35a51fe4e61d
Nik.Brosko <Nikita.brosko@mail.ru>
2022-05-06 17:50:13 +0300

    Create ProductController

---
+++ WebUI/Controllers/ProductController.cs
@@ -0,0 +1,51 @@
+﻿using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using Application.UseCases.Product.Commands.CreateProduct;
+using Application.UseCases.Product.Queries.GetProduct;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class ProductController : ApiControllerBase
+    {
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            try
+            {
+                var product = await Mediator.Send(new GetProductQuery { Id = id });
+
+                ViewBag.Title = product.Name;
+
+                return View(product);
+            }
+            catch (NotFoundException exception)
+            {
+                return View("Error", exception.Message);
+            }
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult Create([FromRoute] int id)
+        {
+            ViewBag.Title = "Create Subcategory";
+
+            return View(new CreateProductCommand { SupplierId = id });
+        }
+
+        [HttpPost("{command}")]
+        public async Task<IActionResult> Create([FromForm] CreateProductCommand command)
+        {
+            try
+            {
+                await Mediator.Send(command);
+            }
+            catch (ItemExistsException exception)
+            {
+                return View("Error", exception.Message);
+            }
+
+            return RedirectToAction("Get", "Supplier", new { id = command.SupplierId });
+        }
+    }
+}
