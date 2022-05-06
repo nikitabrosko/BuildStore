a9e4b34b31772847b321d34e6949e7d53b7d5190
Nik.Brosko <Nikita.brosko@mail.ru>
2022-05-06 17:49:14 +0300

    Create GetSuppliersQuery

---
+++ Application/UseCases/Supplier/Queries/GetSuppliers/GetSuppliersQuery.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using MediatR;
+
+namespace Application.UseCases.Supplier.Queries.GetSuppliers
+{
+    public class GetSuppliersQuery : IRequest<IEnumerable<Domain.Entities.Supplier>>
+    {
+        
+    }
+}
---
+++ Application/UseCases/Supplier/Queries/GetSuppliers/GetSuppliersQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.UseCases.Supplier.Queries.GetSuppliers
+{
+    public class GetSuppliersQueryHandler : IRequestHandler<GetSuppliersQuery, IEnumerable<Domain.Entities.Supplier>>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetSuppliersQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Domain.Entities.Supplier>> Handle(GetSuppliersQuery request, CancellationToken cancellationToken)
+        {
+            return await _context.Suppliers.ToListAsync(cancellationToken);
+        }
+    }
+}
