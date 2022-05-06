66b799b97d262973b3a98917d90efb2d5ee2f724
Nik.Brosko <Nikita.brosko@mail.ru>
2022-05-06 17:49:01 +0300

    Create GetSupplierQuery

---
+++ Application/UseCases/Supplier/Queries/GetSupplier/GetSupplierQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Application.UseCases.Supplier.Queries.GetSupplier
+{
+    public class GetSupplierQuery : IRequest<Domain.Entities.Supplier>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Application/UseCases/Supplier/Queries/GetSupplier/GetSupplierQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.UseCases.Supplier.Queries.GetSupplier
+{
+    public class GetSupplierQueryHandler : IRequestHandler<GetSupplierQuery, Domain.Entities.Supplier>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetSupplierQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Domain.Entities.Supplier> Handle(GetSupplierQuery request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.Suppliers
+                .Include(s => s.Products)
+                .SingleOrDefaultAsync(s => s.Id.Equals(request.Id), cancellationToken);
+
+            if (entity is null)
+            {
+                throw new NotFoundException(nameof(Domain.Entities.Supplier), request.Id);
+            }
+
+            return entity;
+        }
+    }
+}
