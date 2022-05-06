1bfdbe429508ebaab0b6708a5bfc7e4fee962372
Nik.Brosko <Nikita.brosko@mail.ru>
2022-05-06 17:49:40 +0300

    Create GetProductQuery

---
+++ Application/UseCases/Product/Queries/GetProduct/GetProductQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Application.UseCases.Product.Queries.GetProduct
+{
+    public class GetProductQuery : IRequest<Domain.Entities.Product>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Application/UseCases/Product/Queries/GetProduct/GetProductQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using MediatR;
+
+namespace Application.UseCases.Product.Queries.GetProduct
+{
+    public class GetProductQueryHandler : IRequestHandler<GetProductQuery, Domain.Entities.Product>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetProductQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Domain.Entities.Product> Handle(GetProductQuery request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.Products
+                .FindAsync(new object[] {request.Id}, cancellationToken);
+
+            if (entity is null)
+            {
+                throw new NotFoundException(nameof(Product), request.Id);
+            }
+
+            return entity;
+        }
+    }
+}
