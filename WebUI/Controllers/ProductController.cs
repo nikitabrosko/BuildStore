bc5954736cd307410cda311358c88ca97039a1d2
Nik.Brosko <Nikita.brosko@mail.ru>
2022-05-11 17:52:15 +0300

    Update Product and Supplier Controllers

--- WebUI/Controllers/ProductController.cs
+++ WebUI/Controllers/ProductController.cs
@@ -1,6 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Application.Common.Exceptions;
 using Application.UseCases.Product.Commands.CreateProduct;
+using Application.UseCases.Product.Commands.DeleteProduct;
+using Application.UseCases.Product.Commands.UpdateProduct;
 using Application.UseCases.Product.Queries.GetProduct;
 using Microsoft.AspNetCore.Mvc;
 
@@ -46,6 +49,75 @@ namespace WebUI.Controllers
             }
 
             return RedirectToAction("Get", "Supplier", new { id = command.SupplierId });
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> Update([FromRoute] int id)
+        {
+            ViewBag.Title = "Update Product";
+
+            try
+            {
+                var entity = await Mediator.Send(new GetProductQuery { Id = id });
+                var imgSrc = $"data:image/gif;base64,{Convert.ToBase64String(entity.Picture)}";
+
+                ViewBag.Picture = imgSrc;
+
+                var command = new UpdateProductCommand
+                {
+                    Id = id,
+                    Name = entity.Name,
+                    Description = entity.Description,
+                    Price = entity.Price,
+                    Discount = entity.Discount,
+                    QuantityPerUnit = entity.QuantityPerUnit,
+                    Weight = entity.Weight
+                };
+
+                return View(command);
+            }
+            catch (NotFoundException exception)
+            {
+                return View("Error", exception.Message);
+            }
+            catch (ItemExistsException exception)
+            {
+                return View("Error", exception.Message);
+            }
+        }
+
+        [HttpPost("{command}")]
+        public async Task<IActionResult> Update([FromForm] UpdateProductCommand command)
+        {
+            try
+            {
+                var supplierId = await Mediator.Send(command);
+
+                return RedirectToAction("Get", "Supplier", new { id = supplierId });
+            }
+            catch (NotFoundException exception)
+            {
+                return View("Error", exception.Message);
+            }
+            catch (ItemExistsException exception)
+            {
+                return View("Error", exception.Message);
+            }
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            try
+            {
+                var supplierId = await Mediator.Send(new DeleteProductCommand {Id = id});
+
+                return RedirectToAction("Get", "Supplier", new { id = supplierId });
+            }
+            catch (NotFoundException exception)
+            {
+                return View("Error", exception.Message);
+            }
         }
     }
 }
--- WebUI/Controllers/SupplierController.cs
+++ WebUI/Controllers/SupplierController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Threading.Tasks;
 using Application.Common.Exceptions;
 using Application.UseCases.Supplier.Commands.CreateSupplier;
+using Application.UseCases.Supplier.Commands.DeleteSupplier;
+using Application.UseCases.Supplier.Commands.UpdateSupplier;
 using Application.UseCases.Supplier.Queries.GetSupplier;
 using Application.UseCases.Supplier.Queries.GetSuppliers;
 using Microsoft.AspNetCore.Mvc;
@@ -50,6 +52,80 @@ namespace WebUI.Controllers
                 await Mediator.Send(command);
             }
             catch (ItemExistsException exception)
+            {
+                return View("Error", exception.Message);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> Update([FromRoute] int id)
+        {
+            ViewBag.Title = "Update Supplier";
+
+            try
+            {
+                var entity = await Mediator.Send(new GetSupplierQuery { Id = id });
+                
+                var command = new UpdateSupplierCommand
+                {
+                    Id = id,
+                    CompanyName = entity.CompanyName,
+                    Address = entity.Address,
+                    City = entity.City,
+                    Country = entity.Country,
+                    Email = entity.Email,
+                    PhoneNumber = entity.PhoneNumber
+                };
+
+                return View(command);
+            }
+            catch (NotFoundException exception)
+            {
+                return View("Error", exception.Message);
+            }
+            catch (ItemExistsException exception)
+            {
+                return View("Error", exception.Message);
+            }
+        }
+
+        [HttpPost("{command}")]
+        public async Task<IActionResult> Update([FromForm] UpdateSupplierCommand command)
+        {
+            try
+            {
+                await Mediator.Send(command);
+            }
+            catch (NotFoundException exception)
+            {
+                return View("Error", exception.Message);
+            }
+            catch (ItemExistsException exception)
+            {
+                return View("Error", exception.Message);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            ViewBag.Title = "Delete Supplier";
+
+            return View(new DeleteSupplierCommand { Id = id });
+        }
+
+        [HttpPost("{command}")]
+        public async Task<IActionResult> Delete([FromForm] DeleteSupplierCommand command)
+        {
+            try
+            {
+                await Mediator.Send(command);
+            }
+            catch (NotFoundException exception)
             {
                 return View("Error", exception.Message);
             }
