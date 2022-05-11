e47539d2806cfe69e76b0ef1c15410df050fb1a2
Nik.Brosko <Nikita.brosko@mail.ru>
2022-05-11 17:50:06 +0300

    Create DeleteSupplier use case

---
+++ Application/UseCases/Supplier/Commands/DeleteSupplier/DeleteSupplierCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Application.UseCases.Supplier.Commands.DeleteSupplier
+{
+    public class DeleteSupplierCommand : IRequest
+    {
+        public int Id { get; set; }
+
+        public bool ProductsDeletion { get; set; }
+    }
+}
---
+++ Application/UseCases/Supplier/Commands/DeleteSupplier/DeleteSupplierCommandHandler.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.UseCases.Supplier.Commands.DeleteSupplier
+{
+    public class DeleteSupplierCommandHandler : IRequestHandler<DeleteSupplierCommand>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public DeleteSupplierCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(DeleteSupplierCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.Suppliers
+                .Include(s => s.Products)
+                .SingleOrDefaultAsync(s => s.Id.Equals(request.Id), cancellationToken);
+
+            if (entity is null)
+            {
+                throw new NotFoundException(nameof(Domain.Entities.Supplier), request.Id);
+            }
+
+            if (request.ProductsDeletion)
+            {
+                _context.Products.RemoveRange(entity.Products);
+            }
+            else
+            {
+                await _context.Products.ForEachAsync(p => p.Supplier = null, cancellationToken);
+            }
+
+            _context.Suppliers.Remove(entity);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
