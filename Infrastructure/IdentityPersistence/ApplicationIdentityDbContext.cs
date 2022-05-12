447c9ba535e7ca5b15ce85d15ba4a0700730e712
Nik.Brosko <Nikita.brosko@mail.ru>
2022-05-12 21:23:23 +0300

    Create ApplicationIdentityDbContext and IApplicationDbContext interface

---
+++ Infrastructure/IdentityPersistence/ApplicationIdentityDbContext.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Interfaces;
+using Domain.IdentityEntities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.IdentityPersistence
+{
+    public class ApplicationIdentityDbContext : IdentityDbContext<User>, IApplicationIdentityDbContext
+    {
+        public ApplicationIdentityDbContext(DbContextOptions<ApplicationIdentityDbContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
