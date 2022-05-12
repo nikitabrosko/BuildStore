bbc9ebe36a6a13c8f4d19027776daed48d7d2354
Nik.Brosko <Nikita.brosko@mail.ru>
2022-05-12 21:26:00 +0300

    Create GetUsersWithPaginationQuery use case

---
+++ Application/UseCases/Identity/User/Queries/GetUsersWithPagination/GetUsersWithPaginationQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Models;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace Application.UseCases.Identity.User.Queries.GetUsersWithPagination
+{
+    public class GetUsersWithPaginationQueryHandler : IRequestHandler<GetUsersWithPaginationQuery, PaginatedList<Domain.IdentityEntities.User>>
+    {
+        private readonly UserManager<Domain.IdentityEntities.User> _userManager;
+
+        public GetUsersWithPaginationQueryHandler(UserManager<Domain.IdentityEntities.User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public Task<PaginatedList<Domain.IdentityEntities.User>> Handle(GetUsersWithPaginationQuery request, CancellationToken cancellationToken)
+        {
+            var query = _userManager.Users;
+
+            return PaginatedList<Domain.IdentityEntities.User>.CreateAsync(query, request.PageNumber, request.PageSize);
+        }
+    }
+}
