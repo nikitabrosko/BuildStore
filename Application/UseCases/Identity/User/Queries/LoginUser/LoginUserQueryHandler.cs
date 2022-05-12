8a2c2bbcfbe6cb7a33e64ff49098f2b238117113
Nik.Brosko <Nikita.brosko@mail.ru>
2022-05-12 21:26:10 +0300

    Create LoginUserQuery use case

---
+++ Application/UseCases/Identity/User/Queries/LoginUser/LoginUserQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace Application.UseCases.Identity.User.Queries.LoginUser
+{
+    public class LoginUserQueryHandler : IRequestHandler<LoginUserQuery, SignInResult>
+    {
+        private readonly SignInManager<Domain.IdentityEntities.User> _signInManager;
+        private readonly UserManager<Domain.IdentityEntities.User> _userManager;
+
+        public LoginUserQueryHandler(SignInManager<Domain.IdentityEntities.User> signInManager, UserManager<Domain.IdentityEntities.User> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        public async Task<SignInResult> Handle(LoginUserQuery request, CancellationToken cancellationToken)
+        {
+            var entity = await _userManager.FindByNameAsync(request.Name);
+
+            if (entity is null)
+            {
+                throw new NotFoundException(nameof(User), request.Name);
+            }
+
+            return await _signInManager.PasswordSignInAsync(request.Name, request.Password,
+                request.RememberMe, false);
+        }
+    }
+}
---
+++ Application/UseCases/Identity/User/Queries/LoginUser/LoginUserQueryValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace Application.UseCases.Identity.User.Queries.LoginUser
+{
+    public class LoginUserQueryValidator : AbstractValidator<LoginUserQuery>
+    {
+        public LoginUserQueryValidator()
+        {
+            RuleFor(v => v.Name)
+                .MaximumLength(40).WithMessage("Name length cannot be more than 40!")
+                .MinimumLength(2).WithMessage("Name length cannot be less than 2!")
+                .NotEmpty().WithMessage("Name cannot be empty!")
+                .NotNull().WithMessage("Name cannot be null!");
+
+            RuleFor(v => v.Password)
+                .MaximumLength(30).WithMessage("Password length cannot be more than 30!")
+                .MinimumLength(4).WithMessage("Password length cannot be less than 4!")
+                .NotEmpty().WithMessage("Password cannot be empty!")
+                .NotNull().WithMessage("Password cannot be null!");
+        }
+    }
+}
