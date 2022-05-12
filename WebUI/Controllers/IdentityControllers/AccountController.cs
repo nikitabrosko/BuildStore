b714e53c52da702a8d89ea38509e0c2918cd87aa
Nik.Brosko <Nikita.brosko@mail.ru>
2022-05-12 21:26:46 +0300

    Create AccountController and UserController

---
+++ WebUI/Controllers/IdentityControllers/AccountController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using Application.UseCases.Identity.User.Commands.CreateUser;
+using Application.UseCases.Identity.User.Queries.LoginUser;
+using Domain.IdentityEntities;
+using Microsoft.AspNetCore.Identity;
+
+namespace WebUI.Controllers.IdentityControllers
+{
+    public class AccountController : ApiControllerBase
+    {
+        private readonly SignInManager<User> _signInManager;
+
+        public AccountController(SignInManager<User> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Register(string returnUrl = null)
+        {
+            ViewBag.Title = "Register page";
+
+            return View(new CreateUserCommand {ReturnUrl = returnUrl});
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register([FromForm] CreateUserCommand command)
+        {
+            var result = await Mediator.Send(command);
+
+            if (result.Result.Succeeded)
+            {
+                await _signInManager.SignInAsync(result.User, false);
+
+                if (command.ReturnUrl != null)
+                {
+                    return Redirect(command.ReturnUrl);
+                }
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View("IdentityError", result.Result.Errors);
+        }
+
+        [HttpGet]
+        public IActionResult Login(string returnUrl = null)
+        {
+            ViewBag.Title = "Login page";
+
+            return View("_SignInPartial", new LoginUserQuery { ReturnUrl = returnUrl });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login([FromForm] LoginUserQuery query)
+        {
+            try
+            {
+                var result = await Mediator.Send(query);
+
+                if (result.Succeeded)
+                {
+                    if (query.ReturnUrl != null)
+                    {
+                        return Redirect(query.ReturnUrl);
+                    }
+
+                    return RedirectToAction("Index", "Home");
+                }
+
+                return View("Error", "Something went wrong. Check password!");
+            }
+            catch (NotFoundException exception)
+            {
+                return View("Error", exception.Message);
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout(string returnUrl = null)
+        {
+            if (User.IsInRole("admin"))
+            {
+                returnUrl = null;
+            }
+
+            await _signInManager.SignOutAsync();
+
+            if (returnUrl != null)
+            {
+                return Redirect(returnUrl);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ WebUI/Controllers/IdentityControllers/UserController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Application.UseCases.Identity.User.Commands.CreateUser;
+using Application.UseCases.Identity.User.Commands.DeleteUser;
+using Application.UseCases.Identity.User.Commands.UpdateUser;
+using Application.UseCases.Identity.User.Queries.GetUsersWithPagination;
+using Microsoft.AspNetCore.Authorization;
+
+namespace WebUI.Controllers.IdentityControllers
+{
+    [Authorize(Roles = "admin")]
+    public class UserController : ApiControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Index([FromQuery] GetUsersWithPaginationQuery query)
+        {
+            ViewBag.Title = "Users page";
+
+            return View(await Mediator.Send(query));
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewBag.Title = "Create user page";
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromForm] CreateUserCommand command)
+        {
+            await Mediator.Send(command);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Update([FromRoute] string id)
+        {
+            ViewBag.Title = "Update user page";
+
+            return View(new UpdateUserCommand { Id = id });
+        }
+
+        [HttpPost("{command}")]
+        [Route("Update/{command}")]
+        public async Task<IActionResult> Update([FromForm] UpdateUserCommand command)
+        {
+            await Mediator.Send(command);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpDelete("{id}")]
+        [Route("Delete/{id}")]
+        public async Task<IActionResult> Delete([FromRoute] string id)
+        {
+            await Mediator.Send(new DeleteUserCommand { Id = id });
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult ChangePassword(string name)
+        {
+            ViewBag.Title = "Change password page";
+            ViewBag.UserName = name;
+
+            return View();
+        }
+    }
+}
