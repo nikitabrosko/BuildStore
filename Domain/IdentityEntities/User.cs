6065c0c9356e479732ccda989a4fef70027fe282
Nik.Brosko <Nikita.brosko@mail.ru>
2022-05-12 21:19:49 +0300

    Create User identity entity

---
+++ Domain/IdentityEntities/User.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace Domain.IdentityEntities
+{
+    public class User : IdentityUser
+    {
+        public Customer Customer { get; set; }
+    }
+}
