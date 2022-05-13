19632de85c878ec7bf8183b35d73e4297b7b3fd4
Nik.Brosko <Nikita.brosko@mail.ru>
2022-05-13 16:05:50 +0300

    Create CreateRoleCommand use case

---
+++ Application/UseCases/Identity/Role/Commands/CreateRole/CreateRoleCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace Application.UseCases.Identity.Role.Commands.CreateRole
+{
+    public class CreateRoleCommandValidator : AbstractValidator<CreateRoleCommand>
+    {
+        public CreateRoleCommandValidator()
+        {
+            RuleFor(v => v.Name)
+                .MaximumLength(40).WithMessage("Name length cannot be more than 40!")
+                .MinimumLength(2).WithMessage("Name length cannot be less than 2!")
+                .NotEmpty().WithMessage("Name cannot be empty!")
+                .NotNull().WithMessage("Name cannot be null!");
+        }
+    }
+}
