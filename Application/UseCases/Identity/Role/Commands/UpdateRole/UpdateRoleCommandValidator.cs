f0ddd910cfda9429608e08a398465641781f0f25
Nik.Brosko <Nikita.brosko@mail.ru>
2022-05-13 16:06:34 +0300

    Create UpdateRoleCommand use case

---
+++ Application/UseCases/Identity/Role/Commands/UpdateRole/UpdateRoleCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace Application.UseCases.Identity.Role.Commands.UpdateRole
+{
+    public class UpdateRoleCommandValidator : AbstractValidator<UpdateRoleCommand>
+    {
+        public UpdateRoleCommandValidator()
+        {
+            RuleFor(v => v.Name)
+                .MaximumLength(40).WithMessage("Name length cannot be more than 40!")
+                .MinimumLength(2).WithMessage("Name length cannot be less than 2!")
+                .NotEmpty().WithMessage("Name cannot be empty!")
+                .NotNull().WithMessage("Name cannot be null!");
+        }
+    }
+}
