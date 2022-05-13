dcb64631e061e6a5775f78827536d5450000dc0e
Nik.Brosko <Nikita.brosko@mail.ru>
2022-05-14 02:07:46 +0300

    Create GetShoppingCartCommand use case

---
+++ Application/UseCases/ShoppingCart/Queries/GetShoppingCart/GetShoppingCartCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Application.UseCases.ShoppingCart.Queries.GetShoppingCart
+{
+    public class GetShoppingCartCommand : IRequest<Domain.Entities.ShoppingCart>
+    {
+        public string Username { get; set; }
+    }
+}
---
+++ Application/UseCases/ShoppingCart/Queries/GetShoppingCart/GetShoppingCartCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Domain.Entities;
+using Domain.IdentityEntities;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.UseCases.ShoppingCart.Queries.GetShoppingCart
+{
+    public class GetShoppingCartCommandHandler : IRequestHandler<GetShoppingCartCommand, Domain.Entities.ShoppingCart>
+    {
+        private readonly UserManager<User> _userManager;
+
+        public GetShoppingCartCommandHandler(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<Domain.Entities.ShoppingCart> Handle(GetShoppingCartCommand request, CancellationToken cancellationToken)
+        {
+            var entity = (await _userManager.Users
+                .Include(u => u.ShoppingCart)
+                .ThenInclude(s => s.Products)
+                .SingleOrDefaultAsync(s => s.UserName.Equals(request.Username), cancellationToken)).ShoppingCart;
+
+            if (entity is null)
+            {
+                throw new NotFoundException(nameof(Domain.Entities.ShoppingCart), request.Username);
+            }
+
+            return entity;
+        }
+    }
+}
