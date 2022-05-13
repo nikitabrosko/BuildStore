8250a9303b7a0dc5e064b024610643c31ebaa10f
Nik.Brosko <Nikita.brosko@mail.ru>
2022-05-13 16:06:57 +0300

    Create GetRoleWithPaginationQuery use case

---
+++ Application/UseCases/Identity/Role/Queries/GetRolesForSpecifiedUser/GetRolesForSpecifiedUserQuery.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using MediatR;
+
+namespace Application.UseCases.Identity.Role.Queries.GetRolesForSpecifiedUser
+{
+    public class GetRolesForSpecifiedUserQuery : IRequest<IList<string>>
+    {
+        public string UserId { get; set; }
+    }
+}
