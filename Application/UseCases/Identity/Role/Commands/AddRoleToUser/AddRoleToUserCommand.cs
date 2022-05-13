f3234c86bca6d497aab6b39e28e6b78f4c6ba70b
Nik.Brosko <Nikita.brosko@mail.ru>
2022-05-13 16:05:38 +0300

    Create AddRoleToUserCommand use case

---
+++ Application/UseCases/Identity/Role/Commands/AddRoleToUser/AddRoleToUserCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace Application.UseCases.Identity.Role.Commands.AddRoleToUser
+{
+    public class AddRoleToUserCommand : IRequest<IdentityResult>
+    {
+        public string UserId { get; set; }
+
+        public string RoleId { get; set; }
+    }
+}
---
+++ Application/UseCases/Identity/Role/Commands/AddRoleToUser/AddRoleToUserCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace Application.UseCases.Identity.Role.Commands.AddRoleToUser
+{
+    public class AddRoleToUserCommandHandler : IRequestHandler<AddRoleToUserCommand, IdentityResult>
+    {
+        private readonly UserManager<Domain.IdentityEntities.User> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AddRoleToUserCommandHandler(UserManager<Domain.IdentityEntities.User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task<IdentityResult> Handle(AddRoleToUserCommand request, CancellationToken cancellationToken)
+        {
+            var userEntity = await _userManager.FindByIdAsync(request.UserId);
+
+            if (userEntity is null)
+            {
+                throw new NotFoundException(nameof(Domain.IdentityEntities.User), request.UserId);
+            }
+
+            var roleEntity = await _roleManager.FindByIdAsync(request.RoleId);
+
+            if (roleEntity is null)
+            {
+                throw new NotFoundException(nameof(IdentityRole), request.RoleId);
+            }
+
+            return await _userManager.AddToRoleAsync(userEntity, roleEntity.Name);
+        }
+    }
+}
