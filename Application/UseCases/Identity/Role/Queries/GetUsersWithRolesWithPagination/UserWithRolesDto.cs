2d999e69cc6715849a3100334e9fd2f2f4ccb249
Nik.Brosko <Nikita.brosko@mail.ru>
2022-05-13 16:07:49 +0300

    Create GetUsersWithRolesWithPaginationQuery use case

---
+++ Application/UseCases/Identity/Role/Queries/GetUsersWithRolesWithPagination/UserWithRolesDto.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Application.UseCases.Identity.Role.Queries.GetUsersWithRolesWithPagination
+{
+    public class UserWithRolesDto
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public IEnumerable<string> Roles { get; set; }
+    }
+}
