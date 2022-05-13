124ce60b4f86f8739f7fb9c82f3eab23f9759de4
Nik.Brosko <Nikita.brosko@mail.ru>
2022-05-13 16:07:14 +0300

    Create GetRolesWithPaginationQuery use case

---
+++ Application/UseCases/Identity/Role/Queries/GetRolesWithPagination/GetRolesWithPaginationQuery.cs
@@ -0,0 +1,13 @@
+﻿using Application.Common.Models;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace Application.UseCases.Identity.Role.Queries.GetRolesWithPagination
+{
+    public class GetRolesWithPaginationQuery : IRequest<PaginatedList<IdentityRole>>
+    {
+        public int PageNumber { get; set; } = 1;
+
+        public int PageSize { get; set; } = 10;
+    }
+}
---
+++ Application/UseCases/Identity/Role/Queries/GetRolesWithPagination/GetRolesWithPaginationQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Models;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace Application.UseCases.Identity.Role.Queries.GetRolesWithPagination
+{
+    public class GetRolesWithPaginationQueryHandler : IRequestHandler<GetRolesWithPaginationQuery, PaginatedList<IdentityRole>>
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public GetRolesWithPaginationQueryHandler(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public Task<PaginatedList<IdentityRole>> Handle(GetRolesWithPaginationQuery request, CancellationToken cancellationToken)
+        {
+            var query = _roleManager.Roles;
+
+            return PaginatedList<IdentityRole>.CreateAsync(query, request.PageNumber, request.PageSize);
+        }
+    }
+}
