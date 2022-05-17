030d266a3fbe48ba7c125d12020884a862b72f1b
Nik.Brosko <Nikita.brosko@mail.ru>
2022-05-17 19:53:27 +0300

    Create CreateCustomerCommand use case

---
+++ Application/UseCases/Customer/Commands/CreateCustomer/CreateCustomerCommand.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+
+namespace Application.UseCases.Customer.Commands.CreateCustomer
+{
+    public class CreateCustomerCommand : IRequest
+    {
+        public string UserName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
+        public string Phone { get; set; }
+
+        public string CreditCardNumber { get; set; }
+
+        public int CardExpMonth { get; set; }
+
+        public int CardExpYear { get; set; }
+    }
+}
---
+++ Application/UseCases/Customer/Commands/CreateCustomer/CreateCustomerCommandHandler.cs
@@ -0,0 +1,58 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Domain.IdentityEntities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.UseCases.Customer.Commands.CreateCustomer
+{
+    public class CreateCustomerCommandHandler : IRequestHandler<CreateCustomerCommand>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IApplicationIdentityDbContext _identityContext;
+
+        public CreateCustomerCommandHandler(IApplicationDbContext context,
+            IApplicationIdentityDbContext identityContext)
+        {
+            _context = context;
+            _identityContext = identityContext;
+        }
+
+        public async Task<Unit> Handle(CreateCustomerCommand request, CancellationToken cancellationToken)
+        {
+            var userEntity = await _identityContext.Users
+                .Include(u => u.Customer)
+                .SingleOrDefaultAsync(u => u.UserName.Equals(request.UserName), cancellationToken);
+
+            if (userEntity is null)
+            {
+                throw new NotFoundException(nameof(User), request.UserName);
+            }
+
+            var customerEntity = new Domain.Entities.Customer
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Address = request.Address,
+                City = request.City,
+                Country = request.Country,
+                Phone = request.Phone,
+                CreditCardNumber = request.CreditCardNumber,
+                CardExpMonth = request.CardExpMonth,
+                CardExpYear = request.CardExpYear
+            };
+
+            await _context.Customers.AddAsync(customerEntity, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            userEntity.Customer = customerEntity;
+
+            _identityContext.Users.Update(userEntity);
+            await _identityContext.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
