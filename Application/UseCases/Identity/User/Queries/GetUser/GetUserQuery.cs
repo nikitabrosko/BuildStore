f96bf25b2d5ef6e180f4792a5a74777a81fa3035
Nik.Brosko <Nikita.brosko@mail.ru>
2022-05-17 19:54:31 +0300

    Create GetUserQuery user case

---
+++ Application/UseCases/Identity/User/Queries/GetUser/GetUserQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Application.UseCases.Identity.User.Queries.GetUser
+{
+    public class GetUserQuery : IRequest<Domain.IdentityEntities.User>
+    {
+        public string UserName { get; set; }
+    }
+}
---
+++ Application/UseCases/Identity/User/Queries/GetUser/GetUserQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.UseCases.Identity.User.Queries.GetUser
+{
+    public class GetUserQueryHandler : IRequestHandler<GetUserQuery, Domain.IdentityEntities.User>
+    {
+        private readonly IApplicationIdentityDbContext _identityContext;
+
+        public GetUserQueryHandler(IApplicationIdentityDbContext identityContext)
+        {
+            _identityContext = identityContext;
+        }
+
+        public async Task<Domain.IdentityEntities.User> Handle(GetUserQuery request, CancellationToken cancellationToken)
+        {
+            var user = await _identityContext.Users
+                .Include(u => u.Customer)
+                .SingleOrDefaultAsync(u => u.UserName.Equals(request.UserName), cancellationToken);
+
+            return user;
+        }
+    }
+}
