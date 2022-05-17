52a351d9fa3ac6e77fb6b4a342a46459e7ed511d
Nik.Brosko <Nikita.brosko@mail.ru>
2022-05-17 19:54:59 +0300

    Create RemoveProductCommand use case

---
+++ Application/UseCases/ShoppingCart/Commands/RemoveProduct/RemoveProductCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Application.UseCases.ShoppingCart.Commands.RemoveProduct
+{
+    public class RemoveProductCommand : IRequest
+    {
+        public int ProductId { get; set; }
+
+        public string Username { get; set; }
+    }
+}
---
+++ Application/UseCases/ShoppingCart/Commands/RemoveProduct/RemoveProductCommandHandler.cs
@@ -0,0 +1,52 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Domain.IdentityEntities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.UseCases.ShoppingCart.Commands.RemoveProduct
+{
+    public class RemoveProductCommandHandler : IRequestHandler<RemoveProductCommand>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IApplicationIdentityDbContext _identityContext;
+
+        public RemoveProductCommandHandler(IApplicationDbContext context, IApplicationIdentityDbContext identityContext)
+        {
+            _context = context;
+            _identityContext = identityContext;
+        }
+
+        public async Task<Unit> Handle(RemoveProductCommand request, CancellationToken cancellationToken)
+        {
+            var userEntity = await _identityContext.Users
+                .Include(u => u.ShoppingCart)
+                .SingleOrDefaultAsync(u => u.UserName.Equals(request.Username), cancellationToken);
+
+            if (userEntity is null)
+            {
+                throw new NotFoundException(nameof(User), request.Username);
+            }
+
+            var shoppingCartEntity = await _context.ShoppingCarts
+                .Include(s => s.Products)
+                .SingleOrDefaultAsync(s => s.Id.Equals(userEntity.ShoppingCart.Id), cancellationToken);
+
+            var productEntity = await _context.Products
+                .SingleOrDefaultAsync(p => p.Id.Equals(request.ProductId), cancellationToken);
+
+            if (productEntity is null)
+            {
+                throw new NotFoundException(nameof(Domain.Entities.Product), request.ProductId);
+            }
+
+            shoppingCartEntity.Products.Remove(productEntity);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
