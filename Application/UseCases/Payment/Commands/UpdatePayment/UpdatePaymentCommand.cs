6993d308496080834ff2cc212c0ba4822d53e812
Nik.Brosko <Nikita.brosko@mail.ru>
2022-05-20 23:06:21 +0300

    Create UpdatePaymentCommand use case

---
+++ Application/UseCases/Payment/Commands/UpdatePayment/UpdatePaymentCommand.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+using MediatR;
+
+namespace Application.UseCases.Payment.Commands.UpdatePayment
+{
+    public class UpdatePaymentCommand : IRequest
+    {
+        public int? Id { get; set; }
+
+        public bool Allowed { get; set; }
+
+        public PaymentType Type { get; set; }
+
+        public string CreditCardNumber { get; set; }
+
+        public int CardExpMonth { get; set; }
+
+        public int CardExpYear { get; set; }
+    }
+}
---
+++ Application/UseCases/Payment/Commands/UpdatePayment/UpdatePaymentCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.UseCases.Payment.Commands.UpdatePayment
+{
+    public class UpdatePaymentCommandHandler : IRequestHandler<UpdatePaymentCommand>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public UpdatePaymentCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(UpdatePaymentCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.Payments
+                .SingleOrDefaultAsync(p => p.Id.Equals(request.Id), cancellationToken);
+
+            if (entity is null)
+            {
+                throw new NotFoundException(nameof(Domain.Entities.Payment), request.Id);
+            }
+
+            entity.Allowed = request.Allowed;
+            entity.Type = request.Type;
+            entity.CreditCardNumber = request.CreditCardNumber;
+            entity.CardExpMonth = request.CardExpMonth;
+            entity.CardExpYear = request.CardExpYear;
+
+            _context.Payments.Update(entity);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
