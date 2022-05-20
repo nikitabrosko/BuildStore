58b7e72c18b59d0e1e15d6a740f4288cfff6fb24
Nik.Brosko <Nikita.brosko@mail.ru>
2022-05-20 23:06:52 +0300

    Create GetPaymentsWithPaginationQuery use case

---
+++ Application/UseCases/Payment/Queries/GetPaymentsWithPagination/GetPaymentsWithPaginationQuery.cs
@@ -0,0 +1,12 @@
+﻿using Application.Common.Models;
+using MediatR;
+
+namespace Application.UseCases.Payment.Queries.GetPaymentsWithPagination
+{
+    public class GetPaymentsWithPaginationQuery : IRequest<PaginatedList<Domain.Entities.Payment>>
+    {
+        public int PageNumber { get; set; } = 1;
+
+        public int PageSize { get; set; } = 10;
+    }
+}
---
+++ Application/UseCases/Payment/Queries/GetPaymentsWithPagination/GetPaymentsWithPaginationQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Interfaces;
+using Application.Common.Models;
+using MediatR;
+
+namespace Application.UseCases.Payment.Queries.GetPaymentsWithPagination
+{
+    public class GetPaymentsWithPaginationQueryHandler : IRequestHandler<GetPaymentsWithPaginationQuery, PaginatedList<Domain.Entities.Payment>>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetPaymentsWithPaginationQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<PaginatedList<Domain.Entities.Payment>> Handle(GetPaymentsWithPaginationQuery request, CancellationToken cancellationToken)
+        {
+            var query = _context.Payments;
+
+            return await PaginatedList<Domain.Entities.Payment>.CreateAsync(query, request.PageNumber, request.PageSize);
+        }
+    }
+}
