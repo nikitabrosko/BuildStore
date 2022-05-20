e92cc7d44df21f4891a174a404605637edb5122d
Nik.Brosko <Nikita.brosko@mail.ru>
2022-05-20 23:10:23 +0300

    Create Payment controller Create views for Payment

---
+++ WebUI/Controllers/PaymentController.cs
@@ -0,0 +1,126 @@
+﻿using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using Application.UseCases.Order.Commands.UpdateOrder;
+using Application.UseCases.Payment.Commands.CreatePayment;
+using Application.UseCases.Payment.Commands.DeletePayment;
+using Application.UseCases.Payment.Commands.UpdatePayment;
+using Application.UseCases.Payment.Queries.GetPayment;
+using Application.UseCases.Payment.Queries.GetPaymentsWithPagination;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class PaymentController : ApiControllerBase
+    {
+        private string _returnUrl;
+
+        [HttpGet]
+        public async Task<IActionResult> Index([FromQuery] GetPaymentsWithPaginationQuery query)
+        {
+            return View(await Mediator.Send(query));
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            return View(await Mediator.Send(new GetPaymentQuery { Id = id }));
+        }
+
+        [HttpGet("{orderId:int}")]
+        public IActionResult Create(int orderId)
+        {
+            return View(new CreatePaymentCommand {OrderId = orderId});
+        }
+
+        [HttpPost("{command}")]
+        public async Task<IActionResult> Create([FromForm] CreatePaymentCommand command)
+        {
+            var paymentId = await Mediator.Send(command);
+
+            var payment = await Mediator.Send(new GetPaymentQuery {Id = paymentId});
+
+            await Mediator.Send(new UpdateOrderCommand
+            {
+                Id = command.OrderId,
+                Payment = payment
+            });
+
+            return RedirectToAction("Clear", "ShoppingCart");
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> Update([FromRoute] int id, string returnUrl = null)
+        {
+            ViewBag.Title = "Update Payment";
+
+            _returnUrl = returnUrl;
+
+            try
+            {
+                var entity = await Mediator.Send(new GetPaymentQuery { Id = id });
+
+                var command = new UpdatePaymentCommand
+                {
+                    Id = id,
+                    Type = entity.Type,
+                    Allowed = entity.Allowed,
+                    CreditCardNumber = entity.CreditCardNumber,
+                    CardExpMonth = entity.CardExpMonth,
+                    CardExpYear = entity.CardExpYear
+                };
+
+                return View(command);
+            }
+            catch (NotFoundException exception)
+            {
+                return View("Error", exception.Message);
+            }
+        }
+
+        [HttpPost("{command}")]
+        public async Task<IActionResult> Update([FromForm] UpdatePaymentCommand command)
+        {
+            try
+            {
+                await Mediator.Send(command);
+
+                if (_returnUrl != null)
+                {
+                    return Redirect(_returnUrl);
+                }
+
+                return RedirectToAction("Index", "Payment");
+            }
+            catch (NotFoundException exception)
+            {
+                return View("Error", exception.Message);
+            }
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> Delete([FromRoute] int id, string returnUrl = null)
+        {
+            try
+            {
+                await Mediator.Send(new DeletePaymentCommand { Id = id });
+
+                if (returnUrl != null)
+                {
+                    return Redirect(returnUrl);
+                }
+
+                return RedirectToAction("Index", "Payment");
+            }
+            catch (NotFoundException exception)
+            {
+                return View("Error", exception.Message);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult CancelCreation(int orderId)
+        {
+            return RedirectToAction("CancelCreation", "Delivery", new {orderId});
+        }
+    }
+}
