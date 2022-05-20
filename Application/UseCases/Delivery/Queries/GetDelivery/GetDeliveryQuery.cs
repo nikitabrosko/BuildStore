03eb3c3932b9bd29c1fdc88f07f1ce3bb8982ca6
Nik.Brosko <Nikita.brosko@mail.ru>
2022-05-20 23:04:00 +0300

    Create GetDeliveryQuery use case

---
+++ Application/UseCases/Delivery/Queries/GetDelivery/GetDeliveryQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Application.UseCases.Delivery.Queries.GetDelivery
+{
+    public class GetDeliveryQuery : IRequest<Domain.Entities.Delivery>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Application/UseCases/Delivery/Queries/GetDelivery/GetDeliveryQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.UseCases.Delivery.Queries.GetDelivery
+{
+    public class GetDeliveryQueryHandler : IRequestHandler<GetDeliveryQuery, Domain.Entities.Delivery>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetDeliveryQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Domain.Entities.Delivery> Handle(GetDeliveryQuery request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.Deliveries
+                .Include(d => d.Order)
+                .SingleOrDefaultAsync(d => d.Id.Equals(request.Id), cancellationToken);
+
+            if (entity is null)
+            {
+                throw new NotFoundException(nameof(Domain.Entities.Delivery), request.Id);
+            }
+
+            return entity;
+        }
+    }
+}
