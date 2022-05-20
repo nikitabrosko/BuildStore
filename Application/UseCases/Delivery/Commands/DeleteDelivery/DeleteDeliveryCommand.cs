bf6db8aef629b8983ea10322aa7b6fad43b7a767
Nik.Brosko <Nikita.brosko@mail.ru>
2022-05-20 23:03:13 +0300

    Create DeleteDeliveryCommand use case

---
+++ Application/UseCases/Delivery/Commands/DeleteDelivery/DeleteDeliveryCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Application.UseCases.Delivery.Commands.DeleteDelivery
+{
+    public class DeleteDeliveryCommand : IRequest
+    {
+        public int? Id { get; set; }
+    }
+}
---
+++ Application/UseCases/Delivery/Commands/DeleteDelivery/DeleteDeliveryCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.UseCases.Delivery.Commands.DeleteDelivery
+{
+    public class DeleteDeliveryCommandHandler : IRequestHandler<DeleteDeliveryCommand>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public DeleteDeliveryCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(DeleteDeliveryCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.Deliveries
+                .Include(d => d.Order)
+                .ThenInclude(o => o.Delivery)
+                .SingleOrDefaultAsync(d => d.Id.Equals(request.Id), cancellationToken);
+
+            if (entity is null)
+            {
+                throw new NotFoundException(nameof(Domain.Entities.Delivery), request.Id);
+            }
+
+            entity.Order.Delivery = null;
+            entity.Order.DeliveryId = default;
+
+            _context.Deliveries.Remove(entity);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
