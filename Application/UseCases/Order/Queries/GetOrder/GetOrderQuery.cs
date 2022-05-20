e795fb0f51e454521dbd7c66ec94042e45dd97da
Nik.Brosko <Nikita.brosko@mail.ru>
2022-05-20 23:05:12 +0300

    Create GetOrderQuery use case

---
+++ Application/UseCases/Order/Queries/GetOrder/GetOrderQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Application.UseCases.Order.Queries.GetOrder
+{
+    public class GetOrderQuery : IRequest<Domain.Entities.Order>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Application/UseCases/Order/Queries/GetOrder/GetOrderQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.UseCases.Order.Queries.GetOrder
+{
+    public class GetOrderQueryHandler : IRequestHandler<GetOrderQuery, Domain.Entities.Order>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetOrderQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Domain.Entities.Order> Handle(GetOrderQuery request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.Orders
+                .Include(o => o.Delivery)
+                .Include(o => o.Payment)
+                .Include(o => o.Products)
+                .SingleOrDefaultAsync(o => o.Id.Equals(request.Id), cancellationToken);
+
+            if (entity is null)
+            {
+                throw new NotFoundException(nameof(Domain.Entities.Order), request.Id);
+            }
+
+            return entity;
+        }
+    }
+}
