89feb07d12a9fd5d7cb81e3e0d143d173bef3b37
Nik.Brosko <Nikita.brosko@mail.ru>
2022-05-20 23:04:51 +0300

    Create DeleteOrderCommand use case

---
+++ Application/UseCases/Order/Commands/DeleteOrder/DeleteOrderCommandHandler.cs
@@ -0,0 +1,51 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.UseCases.Order.Commands.DeleteOrder
+{
+    public class DeleteOrderCommandHandler : IRequestHandler<DeleteOrderCommand>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public DeleteOrderCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(DeleteOrderCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.Orders
+                .Include(o => o.Delivery)
+                .Include(o => o.Payment)
+                .Include(o => o.Products)
+                .SingleOrDefaultAsync(o => o.Id.Equals(request.Id), cancellationToken);
+
+            if (entity is null)
+            {
+                throw new NotFoundException(nameof(Domain.Entities.Order), request.Id);
+            }
+
+            if (entity.Delivery != null)
+            {
+                _context.Deliveries.Remove(entity.Delivery);
+            }
+
+            if (entity.Payment != null)
+            {
+                _context.Payments.Remove(entity.Payment);
+            }
+
+            entity.Products = null;
+
+            _context.Orders.Remove(entity);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
