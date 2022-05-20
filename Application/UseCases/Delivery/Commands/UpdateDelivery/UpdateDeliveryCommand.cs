6b20f0e658216cd56389cb2839916169663371b1
Nik.Brosko <Nikita.brosko@mail.ru>
2022-05-20 23:03:22 +0300

    Create UpdateDeliveryCommand use case

---
+++ Application/UseCases/Delivery/Commands/UpdateDelivery/UpdateDeliveryCommand.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using MediatR;
+
+namespace Application.UseCases.Delivery.Commands.UpdateDelivery
+{
+    public class UpdateDeliveryCommand : IRequest
+    {
+        public int? Id { get; set; }
+
+        public bool Fulfilled { get; set; }
+
+        public DeliveryType Type { get; set; }
+    }
+}
---
+++ Application/UseCases/Delivery/Commands/UpdateDelivery/UpdateDeliveryCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.UseCases.Delivery.Commands.UpdateDelivery
+{
+    public class UpdateDeliveryCommandHandler : IRequestHandler<UpdateDeliveryCommand>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public UpdateDeliveryCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(UpdateDeliveryCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.Deliveries
+                .SingleOrDefaultAsync(d => d.Id.Equals(request.Id), cancellationToken);
+
+            if (entity is null)
+            {
+                throw new NotFoundException(nameof(Domain.Entities.Delivery), request.Id);
+            }
+
+            entity.Fulfilled = request.Fulfilled;
+            entity.Type = request.Type;
+
+            _context.Deliveries.Update(entity);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
