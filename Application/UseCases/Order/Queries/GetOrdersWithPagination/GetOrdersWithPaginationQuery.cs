f78155d405322dac84c6457a36536116ab896446
Nik.Brosko <Nikita.brosko@mail.ru>
2022-05-20 23:05:52 +0300

    Create GetOrdersWithPaginationQuery use case

---
+++ Application/UseCases/Order/Queries/GetOrdersWithPagination/GetOrdersWithPaginationQuery.cs
@@ -0,0 +1,12 @@
+﻿using Application.Common.Models;
+using MediatR;
+
+namespace Application.UseCases.Order.Queries.GetOrdersWithPagination
+{
+    public class GetOrdersWithPaginationQuery : IRequest<PaginatedList<Domain.Entities.Order>>
+    {
+        public int PageNumber { get; set; } = 1;
+
+        public int PageSize { get; set; } = 10;
+    }
+}
---
+++ Application/UseCases/Order/Queries/GetOrdersWithPagination/GetOrdersWithPaginationQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Interfaces;
+using Application.Common.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.UseCases.Order.Queries.GetOrdersWithPagination
+{
+    public class GetOrdersWithPaginationQueryHandler : IRequestHandler<GetOrdersWithPaginationQuery, PaginatedList<Domain.Entities.Order>>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetOrdersWithPaginationQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<PaginatedList<Domain.Entities.Order>> Handle(GetOrdersWithPaginationQuery request, CancellationToken cancellationToken)
+        {
+            var query = _context.Orders
+                .Include(o => o.Products);
+
+            return await PaginatedList<Domain.Entities.Order>.CreateAsync(query, request.PageNumber, request.PageSize);
+        }
+    }
+}
