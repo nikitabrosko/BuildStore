a6eca35bcc74aa5f888aeb406a12be1eaea4336c
Nik.Brosko <Nikita.brosko@mail.ru>
2022-05-20 23:05:37 +0300

    Create GetOrderForSpecifiedCustomerQuery use case

---
+++ Application/UseCases/Order/Queries/GetOrdersForSpecifiedCustomer/GetOrdersForSpecifiedCustomerQuery.cs
@@ -0,0 +1,14 @@
+﻿using Application.Common.Models;
+using MediatR;
+
+namespace Application.UseCases.Order.Queries.GetOrdersForSpecifiedCustomer
+{
+    public class GetOrdersForSpecifiedCustomerQuery : IRequest<PaginatedList<Domain.Entities.Order>>
+    {
+        public int CustomerId { get; set; }
+
+        public int PageNumber { get; set; } = 1;
+
+        public int PageSize { get; set; } = 10;
+    }
+}
---
+++ Application/UseCases/Order/Queries/GetOrdersForSpecifiedCustomer/GetOrdersForSpecifiedCustomerQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Interfaces;
+using Application.Common.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.UseCases.Order.Queries.GetOrdersForSpecifiedCustomer
+{
+    public class GetOrdersForSpecifiedCustomerQueryHandler : IRequestHandler<GetOrdersForSpecifiedCustomerQuery, PaginatedList<Domain.Entities.Order>>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetOrdersForSpecifiedCustomerQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<PaginatedList<Domain.Entities.Order>> Handle(GetOrdersForSpecifiedCustomerQuery request, CancellationToken cancellationToken)
+        {
+            var query = _context.Orders
+                .Include(o => o.Customer)
+                .Include(o => o.Delivery)
+                .Include(o => o.Payment)
+                .Where(o => o.CustomerId.Equals(request.CustomerId));
+
+            return await PaginatedList<Domain.Entities.Order>.CreateAsync(query, request.PageNumber, request.PageSize);
+        }
+    }
+}
