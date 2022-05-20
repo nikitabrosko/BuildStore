83242830a6864780137d5103ec85d85e126c4d63
Nik.Brosko <Nikita.brosko@mail.ru>
2022-05-20 23:03:48 +0300

    Create GetDeliveriesWithPaginationQuery use case

---
+++ Application/UseCases/Delivery/Queries/GetDeliveriesWithPagination/GetDeliveriesWithPaginationQuery.cs
@@ -0,0 +1,12 @@
+﻿using Application.Common.Models;
+using MediatR;
+
+namespace Application.UseCases.Delivery.Queries.GetDeliveriesWithPagination
+{
+    public class GetDeliveriesWithPaginationQuery : IRequest<PaginatedList<Domain.Entities.Delivery>>
+    {
+        public int PageNumber { get; set; } = 1;
+
+        public int PageSize { get; set; } = 10;
+    }
+}
---
+++ Application/UseCases/Delivery/Queries/GetDeliveriesWithPagination/GetDeliveriesWithPaginationQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Interfaces;
+using Application.Common.Models;
+using MediatR;
+
+namespace Application.UseCases.Delivery.Queries.GetDeliveriesWithPagination
+{
+    public class GetDeliveriesWithPaginationQueryHandler : IRequestHandler<GetDeliveriesWithPaginationQuery, PaginatedList<Domain.Entities.Delivery>>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetDeliveriesWithPaginationQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<PaginatedList<Domain.Entities.Delivery>> Handle(GetDeliveriesWithPaginationQuery request, CancellationToken cancellationToken)
+        {
+            var query = _context.Deliveries;
+
+            return await PaginatedList<Domain.Entities.Delivery>.CreateAsync(query, request.PageNumber, request.PageSize);
+        }
+    }
+}
