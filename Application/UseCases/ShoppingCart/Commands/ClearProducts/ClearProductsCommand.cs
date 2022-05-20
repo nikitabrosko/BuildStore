11c733a15e866e096bf24d7ac6ba79c675ff5043
Nik.Brosko <Nikita.brosko@mail.ru>
2022-05-20 23:07:16 +0300

    Create ShoppingCart\ClearProducts use case

---
+++ Application/UseCases/ShoppingCart/Commands/ClearProducts/ClearProductsCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Application.UseCases.ShoppingCart.Commands.ClearProducts
+{
+    public class ClearProductsCommand : IRequest
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Application/UseCases/ShoppingCart/Commands/ClearProducts/ClearProductsCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.UseCases.ShoppingCart.Commands.ClearProducts
+{
+    public class ClearProductsCommandHandler : IRequestHandler<ClearProductsCommand>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public ClearProductsCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(ClearProductsCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.ShoppingCarts
+                .Include(s => s.Products)
+                .SingleOrDefaultAsync(s => s.Id.Equals(request.Id), cancellationToken);
+
+            if (entity is null)
+            {
+                throw new NotFoundException(nameof(Domain.Entities.ShoppingCart), request.Id);
+            }
+
+            entity.Products.Clear();
+
+            _context.ShoppingCarts.Update(entity);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
