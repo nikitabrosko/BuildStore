f0a4d5ce28bfc98cca7c6a28645f531c867289b4
Nik.Brosko <Nikita.brosko@mail.ru>
2022-05-20 23:06:35 +0300

    Create GetPaymentQuery use case

---
+++ Application/UseCases/Payment/Queries/GetPayment/GetPaymentQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Application.UseCases.Payment.Queries.GetPayment
+{
+    public class GetPaymentQuery : IRequest<Domain.Entities.Payment>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Application/UseCases/Payment/Queries/GetPayment/GetPaymentQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.UseCases.Payment.Queries.GetPayment
+{
+    public class GetPaymentQueryHandler : IRequestHandler<GetPaymentQuery, Domain.Entities.Payment>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetPaymentQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Domain.Entities.Payment> Handle(GetPaymentQuery request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.Payments
+                .Include(p => p.Order)
+                .SingleOrDefaultAsync(p => p.Id.Equals(request.Id), cancellationToken);
+
+            if (entity is null)
+            {
+                throw new NotFoundException(nameof(Domain.Entities.Payment), request.Id);
+            }
+
+            return entity;
+        }
+    }
+}
