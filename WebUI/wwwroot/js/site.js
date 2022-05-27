db0aa51d4084b4c3eb0b5d9ae6f89b8af0e42273
Nik.Brosko <Nikita.brosko@mail.ru>
2022-05-27 18:30:08 +0300

    Update js and css

--- WebUI/wwwroot/js/site.js
+++ WebUI/wwwroot/js/site.js
@@ -1,1 +1,39 @@
-﻿
+﻿$(function () {
+    $('[data-toggle="popover"]').popover();
+});
+
+$(function () {
+    $('.popover-dismiss').popover({
+        trigger: 'focus'
+    });
+});
+
+$('.addToCart').click(function () {
+    var butWrap = $(this).parents('.but-wrap');
+    butWrap.append('<div class="animtocart"></div>');
+});
+
+jQueryAjaxAddProduct = (url) => {
+    $.ajax({
+        type: 'GET',
+        url: url,
+
+        success: function (e) {
+            $('.animtocart').css({
+                'position': 'absolute',
+                'background': '#FF0000',
+                'width': '25px',
+                'height': '25px',
+                'border-radius': '100px',
+                'z-index': '9999999999',
+                'left': e.pageX - 25,
+                'top': e.pageY - 25
+            });
+
+            var cart = $('#shoppingCart').offset();
+            $('.animtocart').animate({ top: cart.top + 'px', left: cart.left + 'px', width: 0, height: 0 }, 800, function () {
+                $(this).remove();
+            });
+        }
+    });
+}
