4c8eadb86461aa7f7160e8c36f957d3b2f6323df
Nik.Brosko <Nikita.brosko@mail.ru>
2022-05-29 19:58:44 +0300

    Create SearchProductWithPaginationQuery use case

---
+++ Application/UseCases/Product/Queries/SearchProductWithPagination/SearchProductWithPaginationQuery.cs
@@ -0,0 +1,14 @@
+﻿using Application.Common.Models;
+using MediatR;
+
+namespace Application.UseCases.Product.Queries.SearchProductWithPagination
+{
+    public class SearchProductWithPaginationQuery : IRequest<PaginatedList<Domain.Entities.Product>>
+    {
+        public string Text { get; set; }
+
+        public int PageNumber { get; set; } = 1;
+
+        public int PageSize { get; set; } = 10;
+    }
+}
---
+++ Application/UseCases/Product/Queries/SearchProductWithPagination/SearchProductWithPaginationQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Interfaces;
+using Application.Common.Models;
+using MediatR;
+
+namespace Application.UseCases.Product.Queries.SearchProductWithPagination
+{
+    public class SearchProductWithPaginationQueryHandler : IRequestHandler<SearchProductWithPaginationQuery, PaginatedList<Domain.Entities.Product>>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public SearchProductWithPaginationQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<PaginatedList<Domain.Entities.Product>> Handle(SearchProductWithPaginationQuery request, CancellationToken cancellationToken)
+        {
+            var query = _context.Products
+                .Where(p => p.Name.StartsWith(request.Text));
+
+            return await PaginatedList<Domain.Entities.Product>.CreateAsync(query, request.PageNumber, request.PageSize);
+        }
+    }
+}
