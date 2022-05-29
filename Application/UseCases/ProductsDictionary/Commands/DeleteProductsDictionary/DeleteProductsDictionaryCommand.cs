d6a7ba13f3e81c04ce33366b23878f54746d69ec
Nik.Brosko <Nikita.brosko@mail.ru>
2022-05-29 19:59:01 +0300

    Create DeleteProductsDictionaryCommand use case

---
+++ Application/UseCases/ProductsDictionary/Commands/DeleteProductsDictionary/DeleteProductsDictionaryCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Application.UseCases.ProductsDictionary.Commands.DeleteProductsDictionary
+{
+    public class DeleteProductsDictionaryCommand : IRequest
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Application/UseCases/ProductsDictionary/Commands/DeleteProductsDictionary/DeleteProductsDictionaryCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.UseCases.ProductsDictionary.Commands.DeleteProductsDictionary
+{
+    public class DeleteProductsDictionaryCommandHandler : IRequestHandler<DeleteProductsDictionaryCommand>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public DeleteProductsDictionaryCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(DeleteProductsDictionaryCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.ProductsDictionaries
+                .Include(p => p.Order)
+                .Include(p => p.Product)
+                .Include(p => p.ShoppingCart)
+                .SingleOrDefaultAsync(p => p.Id.Equals(request.Id), cancellationToken);
+
+            if (entity is null)
+            {
+                throw new NotFoundException(nameof(ProductsDictionary), request.Id);
+            }
+
+            entity.Order = null;
+            entity.Product = null;
+            entity.ShoppingCart = null;
+
+            _context.ProductsDictionaries.Remove(entity);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
