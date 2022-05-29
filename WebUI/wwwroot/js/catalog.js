4a7b6307bece8d7b79468cdc1ea98d0bf7e224b9
Nik.Brosko <Nikita.brosko@mail.ru>
2022-05-29 20:00:23 +0300

    Update site.js Create catalog.js

---
+++ WebUI/wwwroot/js/catalog.js
@@ -0,0 +1,13 @@
+﻿let catalog = document.getElementById("catalog");
+
+catalog.addEventListener("mouseover",
+    function() {
+        catalog.classList.toggle("show");
+    });
+
+catalog.addEventListener("mouseleave",
+    function () {
+        if (catalog.classList.contains("show")) {
+            catalog.classList.remove("show");
+        }
+});
--- WebUI/wwwroot/js/site.js
+++ WebUI/wwwroot/js/site.js
@@ -13,27 +13,78 @@ $('.addToCart').click(function () {
     butWrap.append('<div class="animtocart"></div>');
 });
 
-jQueryAjaxAddProduct = (url) => {
+jQueryAjaxAddProduct = (url, enableAnimation, id) => {
+    $(id).attr("disabled");
+
+    try {
+        $.ajax({
+            type: 'GET',
+            url: url,
+
+            success: function (e) {
+                if (enableAnimation === true) {
+                    $('.animtocart').css({
+                        'position': 'absolute',
+                        'background': '#FF0000',
+                        'width': '25px',
+                        'height': '25px',
+                        'border-radius': '100px',
+                        'z-index': '9999999999',
+                        'left': e.pageX - 25,
+                        'top': e.pageY - 25
+                    });
+
+                    var cart = $('#shoppingCart').offset();
+
+                    $('.animtocart').animate({ top: cart.top + 'px', left: cart.left + 'px', width: 0, height: 0 }, 800, function () {
+                        $(this).remove();
+                    });
+                }
+
+                $(id).removeAttr("disabled");
+            },
+
+            failure: function (fail) {
+                $(id).removeAttr("disabled");
+            },
+
+            error: function (err) {
+                $(id).removeAttr("disabled");
+            }
+        });
+    } catch (e) {
+        $(id).removeAttr("disabled");
+    } 
+    
+}
+
+jQueryAjaxPost = (url, elementId) => {
     $.ajax({
-        type: 'GET',
+        type: 'POST',
         url: url,
 
-        success: function (e) {
-            $('.animtocart').css({
-                'position': 'absolute',
-                'background': '#FF0000',
-                'width': '25px',
-                'height': '25px',
-                'border-radius': '100px',
-                'z-index': '9999999999',
-                'left': e.pageX - 25,
-                'top': e.pageY - 25
-            });
+        success: function (res) {
+            console.log(res);
+            $(elementId).html(res);
+        },
 
-            var cart = $('#shoppingCart').offset();
-            $('.animtocart').animate({ top: cart.top + 'px', left: cart.left + 'px', width: 0, height: 0 }, 800, function () {
-                $(this).remove();
-            });
+        error: function (err) {
+            console.log(err);
+        },
+
+        failure: function(fail) {
+            console.log(fail);
         }
     });
 }
+
+/*jQuerySearch = (url, searchInputId) => {
+    var value = $(searchInputId).val();
+
+    url = url + '/' + value;
+
+    $.ajax({
+        type: 'GET',
+        url: url
+    });
+}*/
