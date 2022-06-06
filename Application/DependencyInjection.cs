705a66289b07612e6cc52f3105767d5b67848720
Nik.Brosko <Nikita.brosko@mail.ru>
2022-06-06 19:48:28 +0300

    Add EmailSender to the DI

--- Application/DependencyInjection.cs
+++ Application/DependencyInjection.cs
@@ -1,14 +1,18 @@
 ﻿using System.Reflection;
 using Application.Common.Behaviors;
+using Application.Common.Interfaces;
+using Application.Common.Models;
 using FluentValidation;
 using MediatR;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Application
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddApplication(this IServiceCollection services)
+        public static IServiceCollection AddApplication(this IServiceCollection services, 
+            IConfiguration configuration)
         {
             services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
             services.AddMediatR(Assembly.GetExecutingAssembly());
@@ -16,6 +20,12 @@ namespace Application
                 typeof(UnhandledExceptionBehavior<,>));
             services.AddTransient(typeof(IPipelineBehavior<,>),
                 typeof(ValidationBehavior<,>));
+
+            services.AddSingleton(configuration
+                .GetSection("EmailConfiguration")
+                .Get<EmailConfiguration>());
+
+            services.AddScoped<IEmailSender, EmailSender>();
 
             return services;
         }
