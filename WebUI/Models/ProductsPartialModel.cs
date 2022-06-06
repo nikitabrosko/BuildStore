036943ff5cc21761ca7cc4804d98dd7d5c0cc0b5
Nik.Brosko <Nikita.brosko@mail.ru>
2022-06-06 19:50:32 +0300

    Create ProductsPartialModel

---
+++ WebUI/Models/ProductsPartialModel.cs
@@ -0,0 +1,12 @@
+﻿using Application.Common.Models;
+using Domain.Entities;
+
+namespace WebUI.Models
+{
+    public class ProductsPartialModel
+    {
+        public PaginatedList<Product> Products { get; set; }
+
+        public Category Category { get; set; }
+    }
+}
