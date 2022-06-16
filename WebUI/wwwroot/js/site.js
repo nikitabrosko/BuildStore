1744422dc735df5b4fedcb8d6b2a76cdfc57c27c
Nik.Brosko <Nikita.brosko@mail.ru>
2022-06-16 22:31:31 +0300

    Rebuild all js files

--- WebUI/wwwroot/js/site.js
+++ WebUI/wwwroot/js/site.js
@@ -1,88 +1,44 @@
-﻿$.validator.addMethod(
-    "regex",
-    function (value, element, regexp) {
-        var re = new RegExp(regexp);
-        return this.optional(element) || re.test(value);
-    },
-    "Check your input!"
-);
-
-$(function () {
-    $('[data-toggle="popover"]').popover();
-});
-
-$(function () {
-    $('.popover-dismiss').popover({
-        trigger: 'focus'
-    });
-});
-
-$('.addToCart').click(function () {
-    var butWrap = $(this).parents('#but-wrap');
-    butWrap.append('<div class="animtocart"></div>');
-});
-
-jQueryAjaxAddProductAndRefreshProductsCount = (addProductUrl, enableAnimation, addProductId,
-    productsCountUrl, productsCountId) => {
-    jQueryAjaxAddProduct(addProductUrl, enableAnimation, addProductId);
-    setTimeout(jQueryAjaxGet, 800, productsCountUrl, productsCountId);
-    }
-
-jQueryAjaxGet = (url, id) => {
+﻿jQueryAjaxGet = (url, id) => {
     $.ajax({
         type: 'GET',
         url: url,
 
         success: function (res) {
-            console.log(res);
             $(id).html(res);
+        },
+
+        error: function (err) {
+            console.log(err);
+        },
+
+        failure: function (fail) {
+            console.log(fail);
         }
     });
 }
 
-jQueryAjaxAddProduct = (url, enableAnimation, id) => {
-    $(id).attr("disabled");
+async function jQueryAjaxGetForShoppingCart(url, id) {
+    await $.ajax({
+        type: 'GET',
+        url: url,
 
-    try {
-        $.ajax({
-            type: 'GET',
-            url: url,
-
-            success: function (e) {
-                if (enableAnimation === true) {
-                    $('.animtocart').css({
-                        'position': 'absolute',
-                        'background': '#007bff',
-                        'width': '25px',
-                        'height': '25px',
-                        'border-radius': '100px',
-                        'z-index': '9999999999',
-                        'left': e.pageX - 25,
-                        'top': e.pageY - 25
-                    });
-
-                    var cart = $('#shoppingCart').offset();
-
-                    $('.animtocart').animate({ top: cart.top + 'px', left: cart.left + 'px', width: 0, height: 0 }, 800, function () {
-                        $(this).remove();
-                    });
-                }
-
-                $(id).removeAttr("disabled");
-            },
-
-            failure: function (fail) {
-                $(id).removeAttr("disabled");
-            },
-
-            error: function (err) {
-                $(id).removeAttr("disabled");
+        success: function (res) {
+            if (res == undefined) {
+                $(id).empty();
             }
-        });
-    } catch (e) {
-        $(id).removeAttr("disabled");
-    } 
-    
+            else {
+                $(id).html(res);
+            }
+        },
+
+        error: function (err) {
+            console.log(err);
+        },
+
+        failure: function (fail) {
+            console.log(fail);
+        }
+    });
 }
 
 jQueryAjaxPost = (url, elementId) => {
@@ -91,7 +47,6 @@ jQueryAjaxPost = (url, elementId) => {
         url: url,
 
         success: function (res) {
-            console.log(res);
             $(elementId).html(res);
         },
 
@@ -105,38 +60,76 @@ jQueryAjaxPost = (url, elementId) => {
     });
 }
 
-jQueryAjaxPostForm = (url, form) => {
-    try {
-        console.log(form);
-        $.ajax({
-            type: 'POST',
-            url: url,
-            data: new FormData(form),
-            processData: false,
+jQueryAjaxDelete = (url, elementId) => {
+    $.ajax({
+        type: 'POST',
+        url: url,
 
-            success: function (res) {
-                console.log(res);
-                alert(res);
-                $("#categoryGet").html(res.html);
-            },
-
-            error: function (err) {
-                console.log(err);
-                alert(err);
-            },
-
-            failure: function (fail) {
-                console.log(fail);
-                alert(fail);
+        success: function (res) {
+            if (res == undefined) {
+                $('#' + elementId).empty();
             }
-        });
-    } catch (e) {
-        console.log(e);
-        alert(e);
-    } 
+            else {
+                $('#' + elementId).html(res);
+            }
+        },
+
+        error: function (err) {
+            console.log(err);
+        },
+
+        failure: function (fail) {
+            console.log(fail);
+        }
+    });
 }
 
-$(".custom-file-input").on("change", function () {
-    var fileName = $(this).val().split("\\").pop();
-    $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+jQueryAjaxPostForm = (form, elementId) => {
+    $.ajax({
+        type: 'POST',
+        url: form.action,
+        data: new FormData(form),
+        processData: false,
+        contentType: false,
+
+        success: function (res) {
+            $(elementId).html(res);
+        },
+
+        error: function (err) {
+            console.log(err);
+        },
+
+        failure: function (fail) {
+            console.log(fail);
+        }
+    })
+}
+
+async function jQueryAjaxForShoppingCart(urlFirst, elementIdFirst, urlSecond, elementIdSecond) {
+    await jQueryAjaxGetForShoppingCart(urlFirst, elementIdFirst);
+    await jQueryAjaxGetForShoppingCart(urlSecond, elementIdSecond);
+
+    var sum = 0;
+    var subsum = 0
+
+    $('td.product-subtotal').each(function () {
+        var str = $(this).html().trim();
+        sum += parseInt(str.substr(4));
+        subsum += parseInt(str.substr(str.length - 2));
+    })
+
+    var subsumstr = subsum.length == 2 ? subsum.length : '0' + subsum;
+
+    $('#shopping-cart-total-sum').html('ИТОГО: BYN ' + sum + ',' + subsumstr);
+}
+
+$(function () {
+    $('[data-toggle="popover"]').popover();
+});
+
+$(function () {
+    $('.popover-dismiss').popover({
+        trigger: 'focus'
+    });
 });
