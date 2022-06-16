fc0d00a0391afb3a903ca9660f3137c8a47533d6
Nik.Brosko <Nikita.brosko@mail.ru>
2022-06-16 22:21:21 +0300

    Create GetRelatedProductsQuery use case

---
+++ Application/UseCases/Product/Queries/GetRelatedProducts/GetRelatedProductsQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using System.Collections.Generic;
+
+namespace Application.UseCases.Product.Queries.GetRelatedProducts
+{
+    public class GetRelatedProductsQuery : IRequest<IEnumerable<Domain.Entities.Product>>
+    {
+        public Domain.Entities.Product Product { get; set; }
+    }
+}
---
+++ Application/UseCases/Product/Queries/GetRelatedProducts/GetRelatedProductsQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.Product.Queries.GetRelatedProducts
+{
+    public class GetRelatedProductsQueryHandler : IRequestHandler<GetRelatedProductsQuery, IEnumerable<Domain.Entities.Product>>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetRelatedProductsQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Domain.Entities.Product>> Handle(GetRelatedProductsQuery request, CancellationToken cancellationToken)
+        {
+            return await _context.Products
+                .Include(p => p.Images)
+                .Where(p => p.Name.Contains(request.Product.Name))
+                .ToListAsync(cancellationToken);
+        }
+    }
+}
