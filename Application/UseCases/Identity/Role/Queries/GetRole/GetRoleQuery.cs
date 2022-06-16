450d274038917458efe4c995b818ce3cc30e43a9
Nik.Brosko <Nikita.brosko@mail.ru>
2022-06-16 22:16:08 +0300

    Create GetRole use case

---
+++ Application/UseCases/Identity/Role/Queries/GetRole/GetRoleQuery.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace Application.UseCases.Identity.Role.Queries.GetRole
+{
+    public class GetRoleQuery : IRequest<IdentityRole>
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ Application/UseCases/Identity/Role/Queries/GetRole/GetRoleQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.Identity.Role.Queries.GetRole
+{
+    public class GetRoleQueryHandler : IRequestHandler<GetRoleQuery, IdentityRole>
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public GetRoleQueryHandler(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public async Task<IdentityRole> Handle(GetRoleQuery request, CancellationToken cancellationToken)
+        {
+            if (request.Id is null)
+            {
+                return await _roleManager.FindByNameAsync(request.Name);
+            }
+            else if (request.Name is null)
+            {
+                return await _roleManager.FindByIdAsync(request.Id);
+            }
+
+            throw new ArgumentException("Both id and Name cannot be in one query!");
+        }
+    }
+}
