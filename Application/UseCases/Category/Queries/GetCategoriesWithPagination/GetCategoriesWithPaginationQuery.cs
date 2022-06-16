8158c7937dc6c8cc2d2eb005989ed213f038eabb
Nik.Brosko <Nikita.brosko@mail.ru>
2022-06-16 22:14:47 +0300

    Create GetCategoriesWithPagination use case

---
+++ Application/UseCases/Category/Queries/GetCategoriesWithPagination/GetCategoriesWithPaginationQuery.cs
@@ -0,0 +1,12 @@
+﻿using Application.Common.Models;
+using MediatR;
+
+namespace Application.UseCases.Category.Queries.GetCategoriesWithPagination
+{
+    public class GetCategoriesWithPaginationQuery : IRequest<PaginatedList<Domain.Entities.Category>>
+    {
+        public int PageNumber { get; set; } = 1;
+
+        public int PageSize { get; set; } = 10;
+    }
+}
---
+++ Application/UseCases/Category/Queries/GetCategoriesWithPagination/GetCategoriesWithPaginationQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using Application.Common.Interfaces;
+using Application.Common.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.Category.Queries.GetCategoriesWithPagination
+{
+    public class GetCategoriesWithPaginationQueryHandler : IRequestHandler<GetCategoriesWithPaginationQuery, PaginatedList<Domain.Entities.Category>>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetCategoriesWithPaginationQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<PaginatedList<Domain.Entities.Category>> Handle(GetCategoriesWithPaginationQuery request, CancellationToken cancellationToken)
+        {
+            var query = _context.Categories
+                .OfType<Domain.Entities.Category>()
+                .Include(c => c.Subcategories)
+                .Include(c => c.Products);
+
+            return await PaginatedList<Domain.Entities.Category>.CreateAsync(query, request.PageNumber, request.PageSize);
+        }
+    }
+}
