2718957aba4c81caa8189c7c92e03f06fd844fb8
Nik.Brosko <Nikita.brosko@mail.ru>
2022-06-16 22:14:55 +0300

    Create SearchCategoriesWithPagination use case

---
+++ Application/UseCases/Category/Queries/SearchCategoriesWithPagination/SearchCategoriesWithPaginationQuery.cs
@@ -0,0 +1,14 @@
+﻿using Application.Common.Models;
+using MediatR;
+
+namespace Application.UseCases.Category.Queries.SearchCategoriesWithPagination
+{
+    public class SearchCategoriesWithPaginationQuery : IRequest<PaginatedList<Domain.Entities.Category>>
+    {
+        public int PageNumber { get; set; } = 1;
+
+        public int PageSize { get; set; } = 10;
+
+        public string Pattern { get; set; }
+    }
+}
---
+++ Application/UseCases/Category/Queries/SearchCategoriesWithPagination/SearchCategoriesWithPaginationQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using Application.Common.Interfaces;
+using Application.Common.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.Category.Queries.SearchCategoriesWithPagination
+{
+    public class SearchCategoriesWithPaginationQueryHandler : IRequestHandler<SearchCategoriesWithPaginationQuery, PaginatedList<Domain.Entities.Category>>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public SearchCategoriesWithPaginationQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<PaginatedList<Domain.Entities.Category>> Handle(SearchCategoriesWithPaginationQuery request, CancellationToken cancellationToken)
+        {
+            var query = _context.Categories
+                .OfType<Domain.Entities.Category>()
+                .Include(c => c.Subcategories)
+                .Include(c => c.Products)
+                .Where(c => c.Name.Contains(request.Pattern));
+
+            return await PaginatedList<Domain.Entities.Category>.CreateAsync(query, request.PageNumber, request.PageSize);
+        }
+    }
+}
