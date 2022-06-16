92ab8b6e05719e8c42e7ccfe932b2b2ab5dad5e9
Nik.Brosko <Nikita.brosko@mail.ru>
2022-06-16 22:22:17 +0300

    Create SearchProductsWithPaginationQuery use case

---
+++ Application/UseCases/Product/Queries/SearchProductsWithPagination/SearchProductsWithPaginationQuery.cs
@@ -0,0 +1,14 @@
+﻿using Application.Common.Models;
+using MediatR;
+
+namespace Application.UseCases.Product.Queries.SearchProductsWithPagination
+{
+    public class SearchProductsWithPaginationQuery : IRequest<PaginatedList<Domain.Entities.Product>>
+    {
+        public string Pattern { get; set; }
+
+        public int PageNumber { get; set; } = 1;
+
+        public int PageSize { get; set; } = 10;
+    }
+}
---
+++ Application/UseCases/Product/Queries/SearchProductsWithPagination/SearchProductsWithPaginationQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Interfaces;
+using Application.Common.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.UseCases.Product.Queries.SearchProductsWithPagination
+{
+    public class SearchProductsWithPaginationQueryHandler : IRequestHandler<SearchProductsWithPaginationQuery, PaginatedList<Domain.Entities.Product>>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public SearchProductsWithPaginationQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<PaginatedList<Domain.Entities.Product>> Handle(SearchProductsWithPaginationQuery request, CancellationToken cancellationToken)
+        {
+            var query = _context.Products
+                .Include(p => p.Category)
+                .Include(p => p.Supplier)
+                .Include(p => p.Images)
+                .Where(p => p.Name.Contains(request.Pattern));
+
+            return await PaginatedList<Domain.Entities.Product>.CreateAsync(query, request.PageNumber, request.PageSize);
+        }
+    }
+}
