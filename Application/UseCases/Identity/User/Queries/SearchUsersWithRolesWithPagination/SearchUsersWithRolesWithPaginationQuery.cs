28766a71c3153b518ec26be3c6c1c187dab6e903
Nik.Brosko <Nikita.brosko@mail.ru>
2022-06-16 22:19:01 +0300

    Create SearchUsersWithRolesWithPaginationQuery use case

---
+++ Application/UseCases/Identity/User/Queries/SearchUsersWithRolesWithPagination/SearchUsersWithRolesWithPaginationQuery.cs
@@ -0,0 +1,15 @@
+﻿using Application.Common.Models;
+using Application.UseCases.Identity.User.Queries.GetUsersWithRolesWithPagination;
+using MediatR;
+
+namespace Application.UseCases.Identity.User.Queries.SearchUsersWithRolesWithPagination
+{
+    public class SearchUsersWithRolesWithPaginationQuery : IRequest<PaginatedList<UserWithRolesDto>>
+    {
+        public int PageNumber { get; set; } = 1;
+
+        public int PageSize { get; set; } = 10;
+
+        public string Pattern { get; set; }
+    }
+}
---
+++ Application/UseCases/Identity/User/Queries/SearchUsersWithRolesWithPagination/SearchUsersWithRolesWithPaginationQueryHandler.cs
@@ -0,0 +1,44 @@
+﻿using Application.Common.Models;
+using Application.UseCases.Identity.User.Queries.GetUsersWithRolesWithPagination;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.Identity.User.Queries.SearchUsersWithRolesWithPagination
+{
+    public class SearchUsersWithRolesWithPaginationQueryHandler : IRequestHandler<SearchUsersWithRolesWithPaginationQuery, PaginatedList<UserWithRolesDto>>
+    {
+        private readonly UserManager<Domain.IdentityEntities.User> _userManager;
+
+        public SearchUsersWithRolesWithPaginationQueryHandler(UserManager<Domain.IdentityEntities.User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<PaginatedList<UserWithRolesDto>> Handle(SearchUsersWithRolesWithPaginationQuery request, CancellationToken cancellationToken)
+        {
+            var query = _userManager.Users
+                .Include(u => u.Customer)
+                .Where(u => u.UserName.Contains(request.Pattern))
+                .Select(user => new UserWithRolesDto
+                {
+                    User = user
+                }).ToList();
+
+            foreach (var user in query)
+            {
+                var entity = await _userManager.FindByIdAsync(user.User.Id);
+
+                user.Roles = await _userManager.GetRolesAsync(entity);
+            }
+
+            return PaginatedList<UserWithRolesDto>.Create(query.AsQueryable(), request.PageNumber, request.PageSize);
+        }
+    }
+}
