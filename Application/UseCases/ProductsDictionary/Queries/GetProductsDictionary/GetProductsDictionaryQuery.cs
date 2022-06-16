06ad2b1629e9f93e23c489737b973814b72ef574
Nik.Brosko <Nikita.brosko@mail.ru>
2022-06-16 22:22:46 +0300

    Create GetProductsDictionaryQuery use case

---
+++ Application/UseCases/ProductsDictionary/Queries/GetProductsDictionary/GetProductsDictionaryQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Application.UseCases.ProductsDictionary.Queries.GetProductsDictionary
+{
+    public class GetProductsDictionaryQuery : IRequest<Domain.Entities.ProductsDictionary>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Application/UseCases/ProductsDictionary/Queries/GetProductsDictionary/GetProductsDictionaryQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.ProductsDictionary.Queries.GetProductsDictionary
+{
+    public class GetProductsDictionaryQueryHandler : IRequestHandler<GetProductsDictionaryQuery, Domain.Entities.ProductsDictionary>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetProductsDictionaryQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Domain.Entities.ProductsDictionary> Handle(GetProductsDictionaryQuery request, CancellationToken cancellationToken)
+        {
+            return await _context.ProductsDictionaries
+                .Include(p => p.Product)
+                    .ThenInclude(p => p.Images)
+                .Include(p => p.ShoppingCart)
+                .Include(p => p.Order)
+                .SingleOrDefaultAsync(p => p.Id.Equals(request.Id), cancellationToken);
+        }
+    }
+}
