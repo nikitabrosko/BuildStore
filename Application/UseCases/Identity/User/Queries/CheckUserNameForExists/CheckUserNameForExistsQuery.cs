e157123267635057f3e3f0f68f4a9f7cb435d431
Nik.Brosko <Nikita.brosko@mail.ru>
2022-06-16 22:17:42 +0300

    Create CheckUserNameForExists use case

---
+++ Application/UseCases/Identity/User/Queries/CheckUserNameForExists/CheckUserNameForExistsQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Application.UseCases.Identity.User.Queries.CheckUserNameForExists
+{
+    public class CheckUserNameForExistsQuery : IRequest<bool>
+    {
+        public string UserName { get; set; }
+    }
+}
---
+++ Application/UseCases/Identity/User/Queries/CheckUserNameForExists/CheckUserNameForExistsQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.Identity.User.Queries.CheckUserNameForExists
+{
+    public class CheckUserNameForExistsQueryHandler : IRequestHandler<CheckUserNameForExistsQuery, bool>
+    {
+        private readonly IApplicationIdentityDbContext _identityContext;
+
+        public CheckUserNameForExistsQueryHandler(IApplicationIdentityDbContext identityContext)
+        {
+            _identityContext = identityContext;
+        }
+
+        public async Task<bool> Handle(CheckUserNameForExistsQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _identityContext.Users
+                .SingleOrDefaultAsync(u => u.UserName.Equals(request.UserName));
+
+            return result is not null;
+        }
+    }
+}
