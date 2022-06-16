a3b123f27ab350e0f77f75ea9de16d909ab0d5f5
Nik.Brosko <Nikita.brosko@mail.ru>
2022-06-16 22:20:56 +0300

    Create GetProductsQuery use case

---
+++ Application/UseCases/Product/Queries/GetProducts/GetProductsQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using System.Collections.Generic;
+
+namespace Application.UseCases.Product.Queries.GetProducts
+{
+    public class GetProductsQuery : IRequest<IEnumerable<Domain.Entities.Product>>
+    {
+        public int? Count { get; set; }
+    }
+}
---
+++ Application/UseCases/Product/Queries/GetProducts/GetProductsQueryHandler.cs
@@ -0,0 +1,44 @@
+﻿using Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.Product.Queries.GetProducts
+{
+    public class GetProductsQueryHandler : IRequestHandler<GetProductsQuery, IEnumerable<Domain.Entities.Product>>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetProductsQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Domain.Entities.Product>> Handle(GetProductsQuery request, CancellationToken cancellationToken)
+        {
+            if (request.Count is not null)
+            {
+                int count = (int)request.Count;
+
+                if (_context.Products.Count() > count)
+                {
+                    return await _context.Products
+                        .Include(p => p.Category)
+                        .Include(p => p.Supplier)
+                        .Include(p => p.Images)
+                        .Take(count)
+                        .ToListAsync();
+                }
+            }
+
+            return await _context.Products
+                .Include(p => p.Category)
+                .Include(p => p.Supplier)
+                .Include(p => p.Images)
+                .ToListAsync();
+        }
+    }
+}
