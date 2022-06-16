d92e32dc97e380021668aceb9200863182b17f04
Nik.Brosko <Nikita.brosko@mail.ru>
2022-06-16 22:21:31 +0300

    Create GetSupplierProductsQuery use case

---
+++ Application/UseCases/Product/Queries/GetSupplierProducts/GetSupplierProductsQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using System.Collections.Generic;
+
+namespace Application.UseCases.Product.Queries.GetSupplierProducts
+{
+    public class GetSupplierProductsQuery : IRequest<IEnumerable<Domain.Entities.Product>>
+    {
+        public Domain.Entities.Supplier Supplier { get; set; }
+    }
+}
---
+++ Application/UseCases/Product/Queries/GetSupplierProducts/GetSupplierProductsQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.Product.Queries.GetSupplierProducts
+{
+    public class GetSupplierProductsQueryHandler : IRequestHandler<GetSupplierProductsQuery, IEnumerable<Domain.Entities.Product>>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetSupplierProductsQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Domain.Entities.Product>> Handle(GetSupplierProductsQuery request, CancellationToken cancellationToken)
+        {
+            return (await _context.Suppliers
+                .Include(s => s.Products)
+                .ThenInclude(p => p.Images)
+                .SingleOrDefaultAsync(s => s.Id.Equals(request.Supplier.Id), cancellationToken))
+                .Products;
+        }
+    }
+}
