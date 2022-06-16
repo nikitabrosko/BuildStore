4a484965fd80fdc94364efd65ada725f11d2e745
Nik.Brosko <Nikita.brosko@mail.ru>
2022-06-16 22:24:11 +0300

    Create GetSuppliersWithPaginationQuery use case

---
+++ Application/UseCases/Supplier/Queries/GetSuppliersWithPagination/GetSuppliersWithPaginationQuery.cs
@@ -0,0 +1,12 @@
+﻿using Application.Common.Models;
+using MediatR;
+
+namespace Application.UseCases.Supplier.Queries.GetSuppliersWithPagination
+{
+    public class GetSuppliersWithPaginationQuery : IRequest<PaginatedList<Domain.Entities.Supplier>>
+    {
+        public int PageNumber { get; set; } = 1;
+
+        public int PageSize { get; set; } = 10;
+    }
+}
---
+++ Application/UseCases/Supplier/Queries/GetSuppliersWithPagination/GetSuppliersWithPaginationQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using Application.Common.Interfaces;
+using Application.Common.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.Supplier.Queries.GetSuppliersWithPagination
+{
+    public class GetSuppliersWithPaginationQueryHandler : IRequestHandler<GetSuppliersWithPaginationQuery, PaginatedList<Domain.Entities.Supplier>>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetSuppliersWithPaginationQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<PaginatedList<Domain.Entities.Supplier>> Handle(GetSuppliersWithPaginationQuery request, CancellationToken cancellationToken)
+        {
+            var query = _context.Suppliers
+                .Include(s => s.Products);
+
+            return await PaginatedList<Domain.Entities.Supplier>.CreateAsync(query, request.PageNumber, request.PageSize);
+        }
+    }
+}
