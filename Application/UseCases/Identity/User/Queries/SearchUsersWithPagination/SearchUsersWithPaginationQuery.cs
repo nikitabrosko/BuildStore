499d6ffde1d79ca56ee3a79227c7bd6069e77725
Nik.Brosko <Nikita.brosko@mail.ru>
2022-06-16 22:18:46 +0300

    Create SearchUsersWithPaginationQuery use case

---
+++ Application/UseCases/Identity/User/Queries/SearchUsersWithPagination/SearchUsersWithPaginationQuery.cs
@@ -0,0 +1,14 @@
+﻿using Application.Common.Models;
+using MediatR;
+
+namespace Application.UseCases.Identity.User.Queries.SearchUsersWithPagination
+{
+    public class SearchUsersWithPaginationQuery : IRequest<PaginatedList<Domain.IdentityEntities.User>>
+    {
+        public int PageNumber { get; set; } = 1;
+
+        public int PageSize { get; set; } = 10;
+
+        public string Pattern { get; set; }
+    }
+}
---
+++ Application/UseCases/Identity/User/Queries/SearchUsersWithPagination/SearchUsersWithPaginationQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using Application.Common.Models;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.Identity.User.Queries.SearchUsersWithPagination
+{
+    public class SearchUsersWithPaginationQueryHandler : IRequestHandler<SearchUsersWithPaginationQuery, PaginatedList<Domain.IdentityEntities.User>>
+    {
+        private readonly UserManager<Domain.IdentityEntities.User> _userManager;
+
+        public SearchUsersWithPaginationQueryHandler(UserManager<Domain.IdentityEntities.User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<PaginatedList<Domain.IdentityEntities.User>> Handle(SearchUsersWithPaginationQuery request, CancellationToken cancellationToken)
+        {
+            var query = _userManager.Users
+                .Where(u => u.UserName.Contains(request.Pattern));
+
+            return await PaginatedList<Domain.IdentityEntities.User>.CreateAsync(query, request.PageNumber, request.PageSize);
+        }
+    }
+}
