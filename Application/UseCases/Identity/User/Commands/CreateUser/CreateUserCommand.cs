bb568ecbc2badb837dee27ba9004e4d4cc30121d
Nik.Brosko <Nikita.brosko@mail.ru>
2022-06-16 22:17:05 +0300

    Update CreateUser use case

--- Application/UseCases/Identity/User/Commands/CreateUser/CreateUserCommand.cs
+++ Application/UseCases/Identity/User/Commands/CreateUser/CreateUserCommand.cs
@@ -9,7 +9,5 @@ namespace Application.UseCases.Identity.User.Commands.CreateUser
         public string Email { get; set; }
 
         public string Password { get; set; }
-
-        public string ReturnUrl { get; set; }
     }
 }
