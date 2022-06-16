f129145b14ee5e3f873d678e86dd2a5044e80285
Nik.Brosko <Nikita.brosko@mail.ru>
2022-06-16 22:16:47 +0300

    Create SearchRolesWithPagination use case

---
+++ Application/UseCases/Identity/Role/Queries/SearchRolesWithPagination/SearchRolesWithPaginationQuery.cs
@@ -0,0 +1,15 @@
+﻿using Application.Common.Models;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace Application.UseCases.Identity.Role.Queries.SearchRolesWithPagination
+{
+    public class SearchRolesWithPaginationQuery : IRequest<PaginatedList<IdentityRole>>
+    {
+        public int PageNumber { get; set; } = 1;
+
+        public int PageSize { get; set; } = 10;
+
+        public string Pattern { get; set; }
+    }
+}
---
+++ Application/UseCases/Identity/Role/Queries/SearchRolesWithPagination/SearchRolesWithPaginationQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using Application.Common.Models;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.Identity.Role.Queries.SearchRolesWithPagination
+{
+    public class SearchRolesWithPaginationQueryHandler : IRequestHandler<SearchRolesWithPaginationQuery, PaginatedList<IdentityRole>>
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public SearchRolesWithPaginationQueryHandler(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public async Task<PaginatedList<IdentityRole>> Handle(SearchRolesWithPaginationQuery request, CancellationToken cancellationToken)
+        {
+            var query = _roleManager.Roles
+                .Where(r => r.Name.Contains(request.Pattern));
+
+            return await PaginatedList<IdentityRole>.CreateAsync(query, request.PageNumber, request.PageSize);
+        }
+    }
+}
