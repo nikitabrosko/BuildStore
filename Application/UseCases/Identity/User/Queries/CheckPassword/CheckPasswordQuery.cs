5f5105d3e4507dfecdcb1bbf85b8a4fd40e03465
Nik.Brosko <Nikita.brosko@mail.ru>
2022-06-16 22:17:31 +0300

    Create CheckPassword use case

---
+++ Application/UseCases/Identity/User/Queries/CheckPassword/CheckPasswordQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Application.UseCases.Identity.User.Queries.CheckPassword
+{
+    public class CheckPasswordQuery : IRequest<bool>
+    {
+        public Domain.IdentityEntities.User User { get; set; }
+
+        public string Password { get; set; }
+    }
+}
---
+++ Application/UseCases/Identity/User/Queries/CheckPassword/CheckPasswordQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.Identity.User.Queries.CheckPassword
+{
+    public class CheckPasswordQueryHandler : IRequestHandler<CheckPasswordQuery, bool>
+    {
+        private readonly UserManager<Domain.IdentityEntities.User> _userManager;
+
+        public CheckPasswordQueryHandler(UserManager<Domain.IdentityEntities.User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<bool> Handle(CheckPasswordQuery request, CancellationToken cancellationToken)
+        {
+            return await _userManager.CheckPasswordAsync(request.User, request.Password);
+        }
+    }
+}
