824450e662da5f4e780a90f893103053c04fa25e
Nik.Brosko <Nikita.brosko@mail.ru>
2022-06-16 22:20:03 +0300

    Create SearchOrdersWithPaginationQuery use case

---
+++ Application/UseCases/Order/Queries/SearchOrdersWithPagination/SearchOrdersWithPaginationQuery.cs
@@ -0,0 +1,14 @@
+﻿using Application.Common.Models;
+using MediatR;
+
+namespace Application.UseCases.Order.Queries.SearchOrdersWithPagination
+{
+    public class SearchOrdersWithPaginationQuery : IRequest<PaginatedList<Domain.Entities.Order>>
+    {
+        public string Pattern { get; set; }
+
+        public int PageNumber { get; set; } = 1;
+
+        public int PageSize { get; set; } = 10;
+    }
+}
---
+++ Application/UseCases/Order/Queries/SearchOrdersWithPagination/SearchOrdersWithPaginationQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using Application.Common.Interfaces;
+using Application.Common.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.Order.Queries.SearchOrdersWithPagination
+{
+    public class SearchOrdersWithPaginationQueryHandler : IRequestHandler<SearchOrdersWithPaginationQuery, PaginatedList<Domain.Entities.Order>>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public SearchOrdersWithPaginationQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<PaginatedList<Domain.Entities.Order>> Handle(SearchOrdersWithPaginationQuery request, CancellationToken cancellationToken)
+        {
+            var query = _context.Orders
+                .Include(o => o.Customer)
+                .Include(o => o.Delivery)
+                .Include(o => o.Payment)
+                .Include(o => o.ProductsDictionary)
+                .ThenInclude(o => o.Product);
+
+            return await PaginatedList<Domain.Entities.Order>.CreateAsync(query, request.PageNumber, request.PageSize);
+        }
+    }
+}
