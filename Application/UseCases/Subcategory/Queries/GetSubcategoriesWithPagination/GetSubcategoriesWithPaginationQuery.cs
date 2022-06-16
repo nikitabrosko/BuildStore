265fe97d17bf60f6fed2ceee4b868aaf6b6d90c6
Nik.Brosko <Nikita.brosko@mail.ru>
2022-06-16 22:23:46 +0300

    Create GetSubcategoryWithPaginationQuery use case

---
+++ Application/UseCases/Subcategory/Queries/GetSubcategoriesWithPagination/GetSubcategoriesWithPaginationQuery.cs
@@ -0,0 +1,12 @@
+﻿using Application.Common.Models;
+using MediatR;
+
+namespace Application.UseCases.Subcategory.Queries.GetSubcategoriesWithPagination
+{
+    public class GetSubcategoriesWithPaginationQuery : IRequest<PaginatedList<Domain.Entities.Subcategory>>
+    {
+        public int PageNumber { get; set; } = 1;
+
+        public int PageSize { get; set; } = 10;
+    }
+}
---
+++ Application/UseCases/Subcategory/Queries/GetSubcategoriesWithPagination/GetSubcategoriesWithPaginationQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using Application.Common.Interfaces;
+using Application.Common.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.Subcategory.Queries.GetSubcategoriesWithPagination
+{
+    public class GetSubcategoriesWithPaginationQueryHandler : IRequestHandler<GetSubcategoriesWithPaginationQuery, PaginatedList<Domain.Entities.Subcategory>>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetSubcategoriesWithPaginationQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<PaginatedList<Domain.Entities.Subcategory>> Handle(GetSubcategoriesWithPaginationQuery request, CancellationToken cancellationToken)
+        {
+            var query = _context.Categories
+                .OfType<Domain.Entities.Subcategory>()
+                .Include(s => s.Category)
+                .Include(s => s.Subcategories)
+                .Include(s => s.Products);
+
+            return await PaginatedList<Domain.Entities.Subcategory>.CreateAsync(query, request.PageNumber, request.PageSize);
+        }
+    }
+}
