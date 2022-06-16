5de4776e954af890fbc2e55a9c52c8c7ceefc5cf
Nik.Brosko <Nikita.brosko@mail.ru>
2022-06-16 22:25:34 +0300

    Create Models for Account

---
+++ WebUI/Models/Account/ModelForLogin.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace WebUI.Models.Account
+{
+    public class ModelForLogin
+    {
+        public IEnumerable<Domain.Entities.Category> Categories { get; set; }
+
+        public Domain.Entities.ShoppingCart ShoppingCart { get; set; }
+    }
+}
---
+++ WebUI/Models/Account/ModelForMyAccount.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace WebUI.Models.Account
+{
+    public class ModelForMyAccount
+    {
+        public IEnumerable<Domain.Entities.Category> Categories { get; set; }
+
+        public Domain.Entities.ShoppingCart ShoppingCart { get; set; }
+
+        public Domain.IdentityEntities.User User { get; set; }
+    }
+}
