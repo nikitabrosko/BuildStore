289487875b15ff51b6f78793e16c3c59bcefc0b3
Nik.Brosko <Nikita.brosko@mail.ru>
2022-06-16 22:18:33 +0300

    Create GetUserViaIdQuery use case

---
+++ Application/UseCases/Identity/User/Queries/GetUserViaId/GetUserViaIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using Application.UseCases.Identity.User.Queries.GetUsersWithRolesWithPagination;
+using MediatR;
+
+namespace Application.UseCases.Identity.User.Queries.GetUserViaId
+{
+    public class GetUserViaIdQuery : IRequest<UserWithRolesDto>
+    {
+        public string Id { get; set; }
+    }
+}
---
+++ Application/UseCases/Identity/User/Queries/GetUserViaId/GetUserViaIdQueryHandler.cs
@@ -0,0 +1,39 @@
+﻿using Application.Common.Interfaces;
+using Application.UseCases.Identity.User.Queries.GetUsersWithRolesWithPagination;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.Identity.User.Queries.GetUserViaId
+{
+    public class GetUserViaIdQueryHandler : IRequestHandler<GetUserViaIdQuery, UserWithRolesDto>
+    {
+        private readonly IApplicationIdentityDbContext _identityContext;
+        private readonly UserManager<Domain.IdentityEntities.User> _userManager;
+
+        public GetUserViaIdQueryHandler(IApplicationIdentityDbContext identityContext,
+            UserManager<Domain.IdentityEntities.User> userManager)
+        {
+            _identityContext = identityContext;
+            _userManager = userManager;
+        }
+
+        public async Task<UserWithRolesDto> Handle(GetUserViaIdQuery request, CancellationToken cancellationToken)
+        {
+            var user = new UserWithRolesDto
+            {
+                User = await _identityContext.Users
+                .Include(u => u.Customer)
+                .ThenInclude(c => c.Orders)
+                .Include(u => u.ShoppingCart)
+                .SingleOrDefaultAsync(u => u.Id.Equals(request.Id), cancellationToken)
+            };
+
+            user.Roles = await _userManager.GetRolesAsync(user.User);
+
+            return user;
+        }
+    }
+}
