5855f891d9b9b85ed85d97da94c41a73c5ee32b6
Nik.Brosko <Nikita.brosko@mail.ru>
2022-06-29 01:46:35 +0300

    Update Supplier commands

--- Application/UseCases/Supplier/Commands/CreateSupplier/CreateSupplierCommandValidator.cs
+++ Application/UseCases/Supplier/Commands/CreateSupplier/CreateSupplierCommandValidator.cs
@@ -7,26 +7,26 @@ namespace Application.UseCases.Supplier.Commands.CreateSupplier
         public CreateSupplierCommandValidator()
         {
             RuleFor(v => v.CompanyName)
-                .MaximumLength(100).WithMessage("CompanyName length cannot be more than 100!")
-                .MinimumLength(3).WithMessage("CompanyName length cannot be less than 3!")
+                .MaximumLength(50).WithMessage("CompanyName length cannot be more than 50!")
+                .MinimumLength(2).WithMessage("CompanyName length cannot be less than 2!")
                 .NotEmpty().WithMessage("CompanyName cannot be empty!")
                 .NotNull().WithMessage("CompanyName cannot be null!");
 
             RuleFor(v => v.Address)
                 .MaximumLength(100).WithMessage("Address length cannot be more than 100!")
-                .MinimumLength(5).WithMessage("Address length cannot be less than 5!")
+                .MinimumLength(2).WithMessage("Address length cannot be less than 2!")
                 .NotEmpty().WithMessage("Address cannot be empty!")
                 .NotNull().WithMessage("Address cannot be null!");
 
             RuleFor(v => v.City)
                 .MaximumLength(50).WithMessage("City length cannot be more than 50!")
-                .MinimumLength(1).WithMessage("City length cannot be less than 1!")
+                .MinimumLength(2).WithMessage("City length cannot be less than 1!")
                 .NotEmpty().WithMessage("City cannot be empty!")
                 .NotNull().WithMessage("City cannot be null!");
 
             RuleFor(v => v.Country)
                 .MaximumLength(50).WithMessage("Country length cannot be more than 50!")
-                .MinimumLength(1).WithMessage("Country length cannot be less than 1!")
+                .MinimumLength(2).WithMessage("Country length cannot be less than 1!")
                 .NotEmpty().WithMessage("Country cannot be empty!")
                 .NotNull().WithMessage("Country cannot be null!");
 
@@ -37,8 +37,6 @@ namespace Application.UseCases.Supplier.Commands.CreateSupplier
                 .NotNull().WithMessage("Email cannot be null!");
 
             RuleFor(v => v.PhoneNumber)
-                .MaximumLength(50).WithMessage("PhoneNumber length cannot be more than 50!")
-                .MinimumLength(1).WithMessage("PhoneNumber length cannot be less than 1!")
                 .NotEmpty().WithMessage("PhoneNumber cannot be empty!")
                 .NotNull().WithMessage("PhoneNumber cannot be null!");
         }
