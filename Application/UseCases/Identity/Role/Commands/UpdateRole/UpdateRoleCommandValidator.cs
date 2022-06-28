5102ae5ada4c5659a05a5ca0ee00d2d26cd18490
Nik.Brosko <Nikita.brosko@mail.ru>
2022-06-29 01:44:09 +0300

    Update Role commands

--- Application/UseCases/Identity/Role/Commands/UpdateRole/UpdateRoleCommandValidator.cs
+++ Application/UseCases/Identity/Role/Commands/UpdateRole/UpdateRoleCommandValidator.cs
@@ -7,8 +7,8 @@ namespace Application.UseCases.Identity.Role.Commands.UpdateRole
         public UpdateRoleCommandValidator()
         {
             RuleFor(v => v.Name)
-                .MaximumLength(40).WithMessage("Name length cannot be more than 40!")
-                .MinimumLength(2).WithMessage("Name length cannot be less than 2!")
+                .MaximumLength(256).WithMessage("Name length cannot be more than 256!")
+                .MinimumLength(1).WithMessage("Name length cannot be less than 1!")
                 .NotEmpty().WithMessage("Name cannot be empty!")
                 .NotNull().WithMessage("Name cannot be null!");
         }
